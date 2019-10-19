03ed0ddc50620a347044208cb444bcda72d017bb
m1yag1 <mike.arbelaez@rice.edu>
2019-10-18 22:12:45 -0500

    update data models so that pdf_url is optional in the post request (#12)

--- backend/app/app/data_models/models.py
+++ backend/app/app/data_models/models.py
@@ -6,25 +6,22 @@ from pydantic import BaseModel
 
 class EventBase(BaseModel):
     collection_id: str = None
-    status_id: int = None
-    pdf_url: Optional[str] = ""
+    status_id: str = None
+    pdf_url: str = None
 
 
 class EventCreate(EventBase):
     collection_id: str
-    status_id: int
+    status_id: str
 
 
 class EventUpdate(BaseModel):
-    status_id: int
-    pdf_url: Optional[str]
+    status_id: str
+    pdf_url: str = None
 
 
 class Event(EventBase):
-    id: int
-    collection_id: str
-    status_id: int
-    pdf_url: Optional[str]
+    id: str
     created_at: datetime
     updated_at: datetime
 
