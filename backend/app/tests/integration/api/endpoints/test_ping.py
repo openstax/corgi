c39720c1a958cd3f6580d01c6bee621986b150b6
m1yag1 <mike.arbelaez@rice.edu>
2019-12-12 20:38:29 -0600

    added enough test code to pass. Errors about test db not existing tho.

--- backend/app/tests/integration/api/endpoints/test_ping.py
+++ backend/app/tests/integration/api/endpoints/test_ping.py
@@ -1,8 +1,14 @@
 import requests
 
+ENDPOINT = "ping"
+
 
 def test_ping_get_request(api_url):
-    endpoint = "ping"
-    url = f"{api_url}/{endpoint}"
+    # GIVEN: An api url to the ping endpoint
+    url = f"{api_url}/{ENDPOINT}"
+
+    # WHEN: A GET request is made to the url
     response = requests.get(url)
+
+    # THEN: A proper response is returned
     assert response.json() == {'message': 'pong'}
