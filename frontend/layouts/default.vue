1c36f8998713e7cd7b191744c760393c0eaa35fb
Marvin Reimer <therealmarv@gmail.com>
2020-01-08 22:52:02 +0200

    remove footer, fix mobile layout

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -5,12 +5,6 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-footer
-      :fixed="fixed"
-      app
-    >
-      <span>&copy; 2019</span>
-    </v-footer>
   </v-app>
 </template>
 
@@ -18,25 +12,7 @@
 export default {
   data () {
     return {
-      clipped: false,
-      drawer: false,
-      fixed: false,
-      items: [
-        {
-          icon: 'mdi-apps',
-          title: 'Welcome',
-          to: '/'
-        },
-        {
-          icon: 'mdi-chart-bubble',
-          title: 'Inspire',
-          to: '/inspire'
-        }
-      ],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'Vuetify.js'
+      title: 'Content Output Producer'
     }
   }
 }
