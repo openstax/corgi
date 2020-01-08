cdf3c7c42c937434a5977018f6c1c4cc158628af
Marvin Reimer <therealmarv@gmail.com>
2020-01-08 22:39:39 +0200

    remove navigation drawer

--- frontend/layouts/default.vue
+++ frontend/layouts/default.vue
@@ -5,23 +5,6 @@
         <nuxt />
       </v-container>
     </v-content>
-    <v-navigation-drawer
-      v-model="rightDrawer"
-      :right="right"
-      temporary
-      fixed
-    >
-      <v-list>
-        <v-list-item @click.native="right = !right">
-          <v-list-item-action>
-            <v-icon light>
-              mdi-repeat
-            </v-icon>
-          </v-list-item-action>
-          <v-list-item-title>Switch drawer (click me)</v-list-item-title>
-        </v-list-item>
-      </v-list>
-    </v-navigation-drawer>
     <v-footer
       :fixed="fixed"
       app
