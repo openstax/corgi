fe03d17d74bc88e0143d546b1dc1171504a2c8c1
Marvin Reimer <therealmarv@gmail.com>
2020-01-10 02:57:23 +0200

    fix warnings, set good title

--- frontend/nuxt.config.js
+++ frontend/nuxt.config.js
@@ -7,7 +7,7 @@ export default {
   ** Headers of the page
   */
   head: {
-    titleTemplate: '%s - ' + process.env.npm_package_name,
+    titleTemplate: 'COPS - Content Output Producers',
     title: process.env.npm_package_name || '',
     meta: [
       { charset: 'utf-8' },
