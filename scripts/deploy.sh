f2e2f426dbe1e14b4fa5f712351694899d15f2f7
m1yag1 <mike.arbelaez@rice.edu>
2020-01-24 16:08:12 -0600

    added docker-compose.deploy file for rolling updates and updated deploy script

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -17,6 +17,7 @@ docker-compose \
 -f docker-compose.deploy.labels.yml \
 -f docker-compose.deploy.networks.yml \
 -f docker-compose.deploy.volumes-placement.yml \
+-f docker-compose.deploy.settings.yml \
 config > docker-stack.yml
 
 docker-auto-labels docker-stack.yml
