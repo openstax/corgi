a476ec630aa04a082b23e3a29defb4c644ac93da
m1yag1 <mike.arbelaez@rice.edu>
2020-02-21 14:56:21 -0600

    wait-for db container to be up before creating the tests database

--- scripts/tests.sh
+++ scripts/tests.sh
@@ -28,6 +28,7 @@ docker-compose \
 docker-compose -f docker-stack.yml build
 docker-compose -f docker-stack.yml down -v --remove-orphans # Remove possibly previous broken stacks left hanging after an error
 docker-compose -f docker-stack.yml up -d
+docker-compose -f docker-stack.yml exec backend-tests wait-for-it -t 10 db:5432
 docker-compose -f docker-stack.yml exec db psql -h db -d postgres -U postgres -c "DROP DATABASE IF EXISTS tests"
 docker-compose -f docker-stack.yml exec db psql -h db -d postgres -U postgres -c "CREATE DATABASE tests ENCODING 'UTF8'"
 docker-compose -f docker-stack.yml exec -T backend-tests ./bin/tests-start.sh
