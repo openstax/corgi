f4e8ffed4bcdeb247123631c3b525c06c0dda181
m1yag1 <mike.arbelaez@rice.edu>
2020-02-24 16:17:14 -0600

    add some messaging about the test report location to tests-start.sh script

--- backend/app/bin/tests-start.sh
+++ backend/app/bin/tests-start.sh
@@ -1,4 +1,8 @@
 #! /usr/bin/env bash
 set -e
 
-wait-for-it -t 10 backend:80 -- pytest ./tests -vvv --junitxml="$TEST_RESULTS"/junit.xml
+TEST_RESULT_PATH=${TEST_RESULT_PATH:-.}
+
+echo "Saving Test Results to : ${TEST_RESULT_PATH}"
+
+wait-for-it -t 10 backend:80 -- pytest ./tests -vvv --junitxml="${TEST_RESULT_PATH}/junit.xml"
