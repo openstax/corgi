80bdc21601689599aa1c1c8878d775853ffe4383
m1yag1 <mike.arbelaez@rice.edu>
2020-03-25 16:14:20 -0500

    registered custom markers for ui and integration tests to the framework in conftest.py

--- backend/app/tests/integration/api/endpoints/test_jobs.py
+++ backend/app/tests/integration/api/endpoints/test_jobs.py
@@ -7,6 +7,7 @@ ENDPOINT = "jobs"
 
 
 # FIXME: Database should be populated with test data.
+@pytest.mark.integration
 @pytest.mark.nondestructive
 def test_jobs_get_request(api_url):
     # GIVEN: An api url to the jobs endpoint
