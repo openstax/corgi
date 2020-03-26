b59737f41b991a8705211d4d8ff31cc26e0eab21
m1yag1 <mike.arbelaez@rice.edu>
2020-03-25 21:50:51 -0500

    Added base region to region/base.py and locators to pages/home.py * Added base region for use in other regions and pages. * Added locator for create_new_pdf_button to pages/home.py * Added locator for new modal region representing the create new pdf job form * Added click_create_new_pdf_button method to click button on home page * Added property method to determine if create pdf modal is displayed

--- backend/app/tests/ui/pages/base.py
+++ backend/app/tests/ui/pages/base.py
@@ -1,10 +1,8 @@
 import pypom
-from selenium.webdriver.common.by import By
 
 
 class Page(pypom.Page):
-    # Default to a 60 second timeout for CNX webview
-    def __init__(self, driver, base_url=None, timeout=90, **url_kwargs):
+    def __init__(self, driver, base_url=None, timeout=30, **url_kwargs):
         super().__init__(driver, base_url, timeout, **url_kwargs)
 
     @property
