c8b3fc58d5c9adfbd90fffc75c915945b3688b75
m1yag1 <mike.arbelaez@rice.edu>
2020-03-25 21:58:52 -0500

    added test_create_new_pdf_button_is_displayed test to tests/home.py

--- backend/app/tests/ui/test_home.py
+++ backend/app/tests/ui/test_home.py
@@ -1,2 +1,15 @@
-def test_home_page_loads():
-    pass
+import pytest
+
+from pages.home import Home
+
+
+@pytest.mark.ui
+@pytest.mark.nondestructive
+def test_create_new_pdf_button_is_displayed(selenium, base_url):
+    # GIVEN: Selenium driver and the base_url
+
+    # WHEN: The Home page is fully loaded
+    home = Home(selenium, base_url).open()
+
+    # THEN: The create new pdf button is displayed
+    assert home.is_create_new_pdf_button_displayed
