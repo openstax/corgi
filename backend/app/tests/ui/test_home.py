a06df937ed0f74191a25d43a604fc54e8e0b0b22
m1yag1 <mike.arbelaez@rice.edu>
2020-03-25 21:59:56 -0500

    added test_Create_pdf_job_modal_form_opens_and_closes (close doesn't work yet)

--- backend/app/tests/ui/test_home.py
+++ backend/app/tests/ui/test_home.py
@@ -13,3 +13,21 @@ def test_create_new_pdf_button_is_displayed(selenium, base_url):
 
     # THEN: The create new pdf button is displayed
     assert home.is_create_new_pdf_button_displayed
+
+
+@pytest.mark.ui
+@pytest.mark.nondestructive
+def test_create_pdf_job_modal_form_opens_and_closes(selenium, base_url):
+    # GIVEN: Selenium driver and the base url
+
+    # WHEN: The Home page is fully loaded
+    # AND: The create new pdf button is clicked
+    home = Home(selenium, base_url).open()
+    modal = home.click_create_new_pdf_button()
+
+    # THEN: The create new pdf modal opens
+    # AND:  The modal closes when cancel is clicked
+    assert home.create_pdf_modal_is_open
+
+    modal.click_cancel_button()
+    assert home.create_pdf_modal_is_closed
