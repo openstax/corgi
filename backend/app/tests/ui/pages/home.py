16234372658488bd6e6b066fb2efd069f7bc4725
m1yag1 <mike.arbelaez@rice.edu>
2020-04-03 23:51:07 -0500

    added locators and click methods to pages/home.py CreatePDFModal region.

--- backend/app/tests/ui/pages/home.py
+++ backend/app/tests/ui/pages/home.py
@@ -33,4 +33,18 @@ class Home(Page):
         return self.CreatePDFModal(self, self.find_element(*self._pdf_job_form_modal_locator))
 
     class CreatePDFModal(Region):
-        pass
+        _modal_cancel_button_locator = (
+            By.CSS_SELECTOR, '#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > button:nth-child(2) > span'
+        )
+        _modal_create_job_button_locator = (
+            By.CSS_SELECTOR,
+            '#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > button:nth-child(3) > span'
+        )
+
+        def click_cancel_button(self):
+            self.find_element(*self._modal_cancel_button_locator)
+            return self
+
+        def click_create_job_button(self):
+            self.find_element(*self._modal_create_job_button_locator)
+            return self
