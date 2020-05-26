de6dedf47ee0afb968585765e51b9481d39d43be
Ripal Nathuji <rnathuji@neoteklabs.com>
2020-05-26 09:39:30 -0500

    Parse module revised date in assemble-book-meta
    REX needs the page revised data in the metadata, and this information
    is available in the module XHTML files generated by `neb assemble`.
    We parse the module files based upon nodes in the collection xhtml
    file and incorporate the revised date as parsed from the files.

--- bakery/src/scripts/assemble-book-metadata.py
+++ bakery/src/scripts/assemble-book-metadata.py
@@ -1,24 +1,48 @@
 import sys
 import json
+from lxml import etree
 from pathlib import Path
 from cnxepub.collation import reconstitute
 from cnxepub.models import flatten_to_documents
+from cnxepub.html_parsers import DocumentMetadataParser
 
 ASSEMBLED_FILENAME = 'collection.assembled.xhtml'
 
-in_dir = Path(sys.argv[1]).resolve(strict=True)
-output_file_path = sys.argv[2]
+def get_module_xhtml_metadata(module_xhtml):
+    """Parse module metadata using XHTML file"""
+    with open(module_xhtml, "r") as module_xhtml_file:
+        html = etree.parse(module_xhtml_file)
+        metadata = DocumentMetadataParser(html)
 
-input_assembled_file = in_dir / ASSEMBLED_FILENAME
+    return metadata
 
-json_data = {}
+def main():
+    in_dir = Path(sys.argv[1]).resolve(strict=True)
+    output_file_path = sys.argv[2]
 
-with open(input_assembled_file, "r") as in_file:
-    binder = reconstitute(in_file)
+    input_assembled_file = in_dir / ASSEMBLED_FILENAME
 
-for doc in flatten_to_documents(binder):
-    abstract = doc.metadata.get("summary")
-    json_data[doc.ident_hash] = { "abstract": abstract }
+    json_data = {}
 
-with open(output_file_path, "w") as out_file:
-    json.dump(json_data, out_file)
+    with open(input_assembled_file, "r") as in_file:
+        binder = reconstitute(in_file)
+
+    for doc in flatten_to_documents(binder):
+        # Parse the metadata from the supporting module XHTML files generated
+        # by neb during assemble to gain access to metadata that is in the
+        # head and doesn't get incorporated into the colllection assembled
+        # XHTML
+        module_xhtml_metadata = get_module_xhtml_metadata(
+            in_dir / f"{doc.id}.xhtml"
+        )
+        abstract = doc.metadata.get("summary")
+        json_data[doc.ident_hash] = {
+            "abstract": abstract,
+            "revised": module_xhtml_metadata.revised
+        }
+
+    with open(output_file_path, "w") as out_file:
+        json.dump(json_data, out_file)
+
+if __name__ == "__main__":
+    main()
