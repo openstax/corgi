d52c9d199f41c0447afff72a6afa13d5b55bb5fe
Otto Mehes <om9@rice.edu>
2020-06-01 17:14:50 -0500

    additional cops tests - test_cops_modal

--- backend/app/tests/ui/pages/home.py
+++ backend/app/tests/ui/pages/home.py
@@ -2,13 +2,42 @@ from pages.base import Page
 from regions.base import Region
 
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 
 
 class Home(Page):
     _create_new_pdf_button_locator = (
-        By.CSS_SELECTOR, '#app > div > main > div > div > div > div > div.text-right > button')
+        By.CSS_SELECTOR,
+        "#app > div > main > div > div > div > div "
+        "> div.text-right "
+        "> button > span",
+    )
     _pdf_job_form_modal_locator = (
-        By.CSS_SELECTOR, '#app > div.v-dialog__content.v-dialog__content--active > div > div'
+        By.CSS_SELECTOR,
+        "#app > div.v-dialog__content.v-dialog__content--active > div > div",
+    )
+    _previous_page_button_locator = (
+        By.CSS_SELECTOR,
+        "#app > div > main > div > div > div > div > div.d-md-flex "
+        "> button:nth-child(1) > span",
+    )
+    _next_page_button_locator = (
+        By.CSS_SELECTOR,
+        "#app > div.v-application--wrap > main > div > div > div "
+        "> div > div.d-md-flex > "
+        "button:nth-child(2) > span ",
+    )
+    _go_button_locator = (
+        By.CSS_SELECTOR,
+        "#app > div.v-application--wrap > main > div > div > div "
+        "> div > div.d-md-flex > button:nth-child(5) > span",
+    )
+    _page_field_locator = (By.CSS_SELECTOR, "#input-17")
+    _no_data_available = (
+        By.CSS_SELECTOR,
+        "# app > div > main > div > div > div > div > "
+        "div.v-data-table.elevation-1.theme--light > div "
+        "> table > tbody > tr > td",
     )
 
     @property
@@ -23,16 +52,74 @@ class Home(Page):
     def create_pdf_modal_is_open(self):
         return self.is_element_displayed(*self._pdf_job_form_modal_locator)
 
+    @property
+    def create_pdf_modal_is_closed(self):
+        return self.is_element_displayed(*self._create_new_pdf_button_locator)
+
     def click_create_new_pdf_button(self):
         self.find_element(*self._create_new_pdf_button_locator).click()
         self.wait.until(lambda _: self.create_pdf_modal_is_open)
-        return self.CreatePDFModal(self, self.find_element(*self._pdf_job_form_modal_locator))
+        return self.CreatePDFModal(
+            self, self.find_element(*self._pdf_job_form_modal_locator)
+        )
+
+    def click_previous_page_button(self):
+        self.find_element(*self._previous_page_button_locator).click()
+        return self
+
+    def click_next_page_button(self):
+        self.find_element(*self._next_page_button_locator).click()
+        return self
+
+    def click_go_button(self):
+        self.find_element(*self._go_button_locator).click()
+        return self
+
+    @property
+    def page_field(self):
+        return self.find_element(*self._page_field_locator)
+
+    def fill_in_page(self, value):
+        self.page_field.send_keys(value)
+        return self
+
+    def no_data_available(self):
+        return self.find_element(*self._no_data_available)
 
     class CreatePDFModal(Region):
         _modal_cancel_button_locator = (
-            By.CSS_SELECTOR, '#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > button:nth-child(2) > span'
+            By.CSS_SELECTOR,
+            "#app > div.v-dialog__content.v-dialog__content--active "
+            "> div > div > "
+            "div.v-card__actions > button:nth-child(2) > span ",
+        )
+        _modal_create_button_locator = (
+            By.CSS_SELECTOR,
+            "#app > div.v-dialog__content.v-dialog__content--active "
+            "> div > div > div.v-card__actions > "
+            "button:nth-child(3) > span ",
         )
 
         def click_cancel_button(self):
             self.find_element(*self._modal_cancel_button_locator).click()
+            return self
+
+        def click_create_button(self):
+            self.find_element(*self._modal_create_button_locator).click()
+            return self
+
+        def fill_collection_id_field(self, value):
+            self.driver.find_element_by_tag_name("body").send_keys(Keys.TAB, value)
+            return self
+
+        def fill_version_field(self, value):
+            self.driver.find_element_by_tag_name("body").send_keys(Keys.TAB, value)
+            return self
+
+        def fill_style_field(self, value):
+            self.driver.find_element_by_tag_name("body").send_keys(Keys.TAB, value)
+            return self
+
+        def fill_server_field(self, value):
+            self.driver.find_element_by_tag_name("body").send_keys(Keys.TAB, value)
             return self
---
+++ backend/app/tests/ui/test_cops_modal.py
@@ -0,0 +1,51 @@
+import pytest
+
+from pages.home import Home
+
+
+@pytest.mark.smoke
+@pytest.mark.ui
+@pytest.mark.nondestructive
+def test_modal_remains_open_with_data_fields_empty(selenium, base_url):
+    # GIVEN: Selenium driver and the base url
+
+    # WHEN: The Home page is fully loaded
+    home = Home(selenium, base_url).open()
+    # AND: The create new pdf button is clicked
+    modal = home.click_create_new_pdf_button()
+
+    # AND: Create button is clicked when data fields are empty
+    modal.click_create_button()
+
+    # THEN: The create new pdf modal stays open
+    assert home.create_pdf_modal_is_open
+
+
+@pytest.mark.smoke
+@pytest.mark.ui
+@pytest.mark.nondestructive
+@pytest.mark.parametrize("colid", ["col26069"])
+@pytest.mark.parametrize("vers", ["latest"])
+@pytest.mark.parametrize("style", ["chemistry"])
+@pytest.mark.parametrize("serv", ["staging"])
+def test_modal_create_pdf_cancel(selenium, base_url, colid, vers, style, serv):
+    # GIVEN: Selenium driver and the base url
+
+    # WHEN: The Home page is fully loaded
+    home = Home(selenium, base_url).open()
+
+    # AND: Modal window opens
+    modal = home.click_create_new_pdf_button()
+
+    # AND: Data is entered into the four fields
+    modal.fill_collection_id_field(colid)
+    modal.fill_version_field(vers)
+    modal.fill_style_field(style)
+    modal.fill_server_field(serv)
+
+    # AND: Cancel button is clicked
+    modal.click_cancel_button()
+
+    # THEN: The modal window closes
+    assert home.create_pdf_modal_is_closed
+    assert home.is_create_new_pdf_button_displayed
