34bd4ebb72c10e20981fb4e6e71520a07e9e0e78
Thomas Williamson <tomjw64@users.noreply.github.com>
2020-06-03 17:11:21 -0500

    Add promotion script for staging -> prod (#106)
    * Add promotion script for staging -> prod
    
    * Remove default of latest for TAG variable in deploy script
    
    The deploy script will fail if TAG is not given and prompt the user to
    set the variable.
    
    * Add docs for changed deployment procedures

---
+++ scripts/vars.prod.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export DOMAIN="cops.openstax.org"
+export STACK_NAME="cops_prod"
+export TRAEFIK_TAG="traefik-public"
---
+++ scripts/vars.staging.sh
@@ -0,0 +1,4 @@
+#!/usr/bin/env bash
+export DOMAIN="cops-staging.openstax.org"
+export STACK_NAME="cops_stag"
+export TRAEFIK_TAG="traefik-staging"
