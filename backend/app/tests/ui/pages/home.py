b89ca452d7ab6663ac49acfe24aa98784dd9b419
m1yag1 <mike.arbelaez@rice.edu>
2020-06-04 17:50:07 -0500

    Refactored /backend/app/pages/home.py to use new class names
    * Updated locators for the jobs-modal, create-job-button, and
      cancel-jobs-button.

--- backend/app/tests/ui/pages/home.py
+++ backend/app/tests/ui/pages/home.py
@@ -5,15 +5,13 @@ from selenium.webdriver.common.by import By
 
 
 class Home(Page):
-    _create_new_pdf_button_locator = (
-        By.CSS_SELECTOR, '#app > div > main > div > div > div > div > div.text-right > button')
-    _pdf_job_form_modal_locator = (
-        By.CSS_SELECTOR, '#app > div.v-dialog__content.v-dialog__content--active > div > div'
-    )
+    _create_new_pdf_button_locator = (By.CLASS_NAME, 'create-job-button')
+    _pdf_job_form_modal_locator = (By.CLASS_NAME, 'job-modal')
 
     @property
     def loaded(self):
-        return self.is_create_new_pdf_button_displayed
+        return (self.is_create_new_pdf_button_displayed and
+                not self.create_pdf_modal_is_open)
 
     @property
     def is_create_new_pdf_button_displayed(self):
@@ -26,13 +24,16 @@ class Home(Page):
     def click_create_new_pdf_button(self):
         self.find_element(*self._create_new_pdf_button_locator).click()
         self.wait.until(lambda _: self.create_pdf_modal_is_open)
-        return self.CreatePDFModal(self, self.find_element(*self._pdf_job_form_modal_locator))
+        return self.CreatePDFModal(self, self.find_element(
+            *self._pdf_job_form_modal_locator))
 
     class CreatePDFModal(Region):
-        _modal_cancel_button_locator = (
-            By.CSS_SELECTOR, '#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > button:nth-child(2) > span'
-        )
+        _modal_cancel_button_locator = (By.CSS_SELECTOR, '.job-cancel-button > span')
+
+        @property
+        def cancel_button(self):
+            return self.find_element(*self._modal_cancel_button_locator)
 
         def click_cancel_button(self):
-            self.find_element(*self._modal_cancel_button_locator).click()
-            return self
+            self.cancel_button.click()
+            return self.page.wait_for_page_to_load()
