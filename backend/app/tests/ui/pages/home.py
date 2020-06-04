a25ce6e7ac6d0dabf69d12bf79836f0eff8910c3
m1yag1 <mike.arbelaez@rice.edu>
2020-06-04 17:59:53 -0500

    rename instantances of PDF to Job to better represent intent

--- backend/app/tests/ui/pages/home.py
+++ backend/app/tests/ui/pages/home.py
@@ -5,29 +5,29 @@ from selenium.webdriver.common.by import By
 
 
 class Home(Page):
-    _create_new_pdf_button_locator = (By.CLASS_NAME, 'create-job-button')
+    _create_new_job_button_locator = (By.CLASS_NAME, 'create-job-button')
     _pdf_job_form_modal_locator = (By.CLASS_NAME, 'job-modal')
 
     @property
     def loaded(self):
-        return self.is_create_new_pdf_button_displayed
+        return self.is_create_new_job_button_displayed
 
 
     @property
-    def is_create_new_pdf_button_displayed(self):
-        return self.is_element_displayed(*self._create_new_pdf_button_locator)
+    def is_create_new_job_button_displayed(self):
+        return self.is_element_displayed(*self._create_new_job_button_locator)
 
     @property
-    def create_pdf_modal_is_open(self):
+    def create_job_modal_is_open(self):
         return self.is_element_displayed(*self._pdf_job_form_modal_locator)
 
-    def click_create_new_pdf_button(self):
-        self.find_element(*self._create_new_pdf_button_locator).click()
-        self.wait.until(lambda _: self.create_pdf_modal_is_open)
-        return self.CreatePDFModal(self, self.find_element(
+    def click_create_new_job_button(self):
+        self.find_element(*self._create_new_job_button_locator).click()
+        self.wait.until(lambda _: self.create_job_modal_is_open)
+        return self.CreateJobModal(self, self.find_element(
             *self._pdf_job_form_modal_locator))
 
-    class CreatePDFModal(Region):
+    class CreateJobModal(Region):
         _modal_cancel_button_locator = (By.CSS_SELECTOR, '.job-cancel-button > span')
 
         @property
@@ -36,4 +36,4 @@ class Home(Page):
 
         def click_cancel_button(self):
             self.cancel_button.click()
-            self.wait.until(lambda _: not self.page.create_pdf_modal_is_open)
+            self.wait.until(lambda _: not self.page.create_job_modal_is_open)
