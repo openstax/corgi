0ed454f660428d127f077e751ee356edbe3b27a5
m1yag1 <mike.arbelaez@rice.edu>
2020-06-05 09:26:14 -0500

    Rename pdf modal to better convey the intent of the "job manager"  (#112)
    * Added class name `jobs-modal` to modal which will be used by UI tests to detect if
      it's open/closed.
    * Added class name `create-job-button` to identify the create pdf job
      button for UI tests.
    * Added class name `cancel-job-button` to identify the cancel job button
      which is used to close the modal.
    * Refactored /backend/app/pages/home.py to use new class names
    * Updated locators in for the jobs-modal, create-job-button, and
      cancel-jobs-button.
    * added junit test artifact to .gitignore file
    * updated frontend/package-lock.json after running yarn install
    * rename instances of PDF to Job to better represent the intent.

--- backend/app/tests/ui/pages/home.py
+++ backend/app/tests/ui/pages/home.py
@@ -5,34 +5,35 @@ from selenium.webdriver.common.by import By
 
 
 class Home(Page):
-    _create_new_pdf_button_locator = (
-        By.CSS_SELECTOR, '#app > div > main > div > div > div > div > div.text-right > button')
-    _pdf_job_form_modal_locator = (
-        By.CSS_SELECTOR, '#app > div.v-dialog__content.v-dialog__content--active > div > div'
-    )
+    _create_new_job_button_locator = (By.CLASS_NAME, 'create-job-button')
+    _pdf_job_form_modal_locator = (By.CLASS_NAME, 'job-modal')
 
     @property
     def loaded(self):
-        return self.is_create_new_pdf_button_displayed
+        return self.is_create_new_job_button_displayed
+
 
     @property
-    def is_create_new_pdf_button_displayed(self):
-        return self.is_element_displayed(*self._create_new_pdf_button_locator)
+    def is_create_new_job_button_displayed(self):
+        return self.is_element_displayed(*self._create_new_job_button_locator)
 
     @property
-    def create_pdf_modal_is_open(self):
+    def create_job_modal_is_open(self):
         return self.is_element_displayed(*self._pdf_job_form_modal_locator)
 
-    def click_create_new_pdf_button(self):
-        self.find_element(*self._create_new_pdf_button_locator).click()
-        self.wait.until(lambda _: self.create_pdf_modal_is_open)
-        return self.CreatePDFModal(self, self.find_element(*self._pdf_job_form_modal_locator))
+    def click_create_new_job_button(self):
+        self.find_element(*self._create_new_job_button_locator).click()
+        self.wait.until(lambda _: self.create_job_modal_is_open)
+        return self.CreateJobModal(self, self.find_element(
+            *self._pdf_job_form_modal_locator))
 
-    class CreatePDFModal(Region):
-        _modal_cancel_button_locator = (
-            By.CSS_SELECTOR, '#app > div.v-dialog__content.v-dialog__content--active > div > div > div.v-card__actions > button:nth-child(2) > span'
-        )
+    class CreateJobModal(Region):
+        _modal_cancel_button_locator = (By.CLASS_NAME, 'job-cancel-button')
+
+        @property
+        def cancel_button(self):
+            return self.find_element(*self._modal_cancel_button_locator)
 
         def click_cancel_button(self):
-            self.find_element(*self._modal_cancel_button_locator).click()
-            return self
+            self.cancel_button.click()
+            self.wait.until(lambda _: not self.page.create_job_modal_is_open)
