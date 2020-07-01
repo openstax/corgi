d58bd6d2fc30ec97d2b48d56d824f808e90d34c2
Ripal Nathuji <rnathuji@neoteklabs.com>
2020-07-01 12:04:02 -0500

    Add validate-xhtml task definition

---
+++ bakery/src/tasks/validate-xhtml.js
@@ -0,0 +1,44 @@
+const dedent = require('dedent')
+
+const { constructImageSource } = require('../task-util/task-util')
+
+const task = (taskArgs) => {
+  const { inputSource, inputPath } = taskArgs
+  const imageDefault = {
+    name: 'openstax/xhtml-validator',
+    tag: 'master'
+  }
+  const imageOverrides = taskArgs != null && taskArgs.image != null ? taskArgs.image : {}
+  const imageSource = constructImageSource({ ...imageDefault, ...imageOverrides })
+
+  return {
+    task: 'validate xhtml',
+    config: {
+      platform: 'linux',
+      image_resource: {
+        type: 'docker-image',
+        source: imageSource
+      },
+      inputs: [
+        { name: 'book' },
+        { name: `${inputSource}` }
+      ],
+      run: {
+        path: '/bin/bash',
+        args: [
+          '-cxe',
+          dedent`
+          collection_id="$(cat book/collection_id)"
+          for xhtmlfile in "${inputSource}/$collection_id/"${inputPath}
+          do
+            java -cp /xhtml-validator.jar org.openstax.xml.Main "$xhtmlfile" link-to-duplicate-id
+            java -cp /xhtml-validator.jar org.openstax.xml.Main "$xhtmlfile" broken-link
+          done
+        `
+        ]
+      }
+    }
+  }
+}
+
+module.exports = task
