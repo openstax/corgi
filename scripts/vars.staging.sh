be66eeebda9c1043d4de3fc5559daed0e12e7196
Brenda Nguyen <bn16@rice.edu>
2020-10-16 13:49:50 -0500

    will clean up docs after my latest deploy

--- scripts/vars.staging.sh
+++ scripts/vars.staging.sh
@@ -2,3 +2,10 @@
 export DOMAIN="cops-staging.openstax.org"
 export STACK_NAME="cops_stag"
 export TRAEFIK_TAG="traefik-staging"
+
+NEWLINE=$'\n'
+echo "${NEWLINE}The following environment variables were set:${NEWLINE}"
+echo "DOMAIN=$DOMAIN"
+echo "STACK_NAME=$STACK_NAME"
+echo "TRAEFIK_TAG=$TRAEFIK_TAG"
+echo "${NEWLINE}"
