54dbf06c6ca9e41754218c4e8dd597684238e4bc
Ripal Nathuji <rnathuji@neoteklabs.com>
2020-10-26 16:27:56 -0500

    Add docker user authentication for bakery pipelines

--- bakery/src/task-util/task-util.js
+++ bakery/src/task-util/task-util.js
@@ -1,7 +1,9 @@
-const constructImageSource = ({ registry, name, tag }) => {
+const constructImageSource = ({ registry, name, tag, username, password }) => {
   const source = {}
   if (name == null) { return null }
   if (tag != null) { source.tag = tag }
+  if (username != null) { source.username = username }
+  if (password != null) { source.password = password }
   if (registry != null) {
     source.repository = `${registry}/${name}`
     source.insecure_registries = [registry]
