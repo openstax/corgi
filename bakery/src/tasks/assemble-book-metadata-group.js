aabc5d065bc7c1aabcbd0dc735393a86bfcede32
Chris Kline <ckline.tryptic@gmail.com>
2020-11-09 21:00:01 +0000

    add bake_book_metadata.sh, and bug fix

--- bakery/src/tasks/assemble-book-metadata-group.js
+++ bakery/src/tasks/assemble-book-metadata-group.js
@@ -1,4 +1,5 @@
 const { constructImageSource } = require('../task-util/task-util')
+
 const fs = require('fs')
 const path = require('path')
 
--- bakery/src/tasks/bake-book-metadata-group.js
+++ bakery/src/tasks/bake-book-metadata-group.js
@@ -1,6 +1,7 @@
-const dedent = require('dedent')
-
 const { constructImageSource } = require('../task-util/task-util')
+
+const fs = require('fs')
+const path = require('path')
 
 const task = (taskArgs) => {
   const imageDefault = {
@@ -14,6 +15,7 @@ const task = (taskArgs) => {
   const bakedInput = 'baked-book-group'
   const assembledMetaInput = 'assembled-book-metadata-group'
   const bakedMetaOutput = 'baked-book-metadata-group'
+  const shellScript = fs.readFileSync(path.resolve(__dirname, '../scripts/bake_book_metadata_group.sh'), { encoding: "utf-8" })
 
   return {
     task: 'bake book metadata',
@@ -29,30 +31,17 @@ const task = (taskArgs) => {
         { name: assembledMetaInput }
       ],
       outputs: [{ name: bakedMetaOutput }],
+      params: {
+        BAKED_META_OUTPUT: bakedMetaOutput,
+        BAKED_INPUT: bakedInput,
+        FETCHED_INPUT: fetchedInput,
+        ASSEMBLED_META_INPUT: assembledMetaInput
+      },
       run: {
         path: '/bin/bash',
         args: [
           '-cxe',
-          dedent`
-          exec 2> >(tee ${bakedMetaOutput}/stderr >&2)
-          for collection in $(find "${bakedInput}/" -path *.baked.xhtml -type f); do
-            slug_name=$(basename "$collection" | awk -F'[.]' '{ print $1; }')
-
-            book_metadata="${fetchedInput}/raw/metadata/$slug_name.metadata.json"
-            book_uuid="$(cat $book_metadata | jq -r '.id')"
-            book_version="$(cat $book_metadata | jq -r '.version')"
-            book_legacy_id="$(cat $book_metadata | jq -r '.legacy_id')"
-            book_legacy_version="$(cat $book_metadata | jq -r '.legacy_version')"
-            book_ident_hash="$book_uuid@$book_version"
-            book_license="$(cat $book_metadata | jq '.license')"
-            book_slugs_file="${fetchedInput}/book-slugs.json"
-            cat "${assembledMetaInput}/$slug_name.assembled-metadata.json" | \
-                jq --arg ident_hash "$book_ident_hash" --arg uuid "$book_uuid" --arg version "$book_version" --argjson license "$book_license" \
-                --arg legacy_id "$book_legacy_id" --arg legacy_version "$book_legacy_version" \
-                '. + {($ident_hash): {id: $uuid, version: $version, license: $license, legacy_id: $legacy_id, legacy_version: $legacy_version}}' > "/tmp/$slug_name.baked-input-metadata.json"
-            bake-meta /tmp/$slug_name.baked-input-metadata.json "${bakedInput}/$slug_name.baked.xhtml" "$book_uuid" "$book_slugs_file" "${bakedMetaOutput}/$slug_name.baked-metadata.json"
-          done
-          `
+          shellScript
         ]
       }
     }
