2133818f2d1f958ad83e30dafa2893e2ecd601bd
Brenda Nguyen <bn16@rice.edu>
2020-12-07 10:33:06 -0800

    lint pipeline file

--- bakery/src/pipelines/git-web.js
+++ bakery/src/pipelines/git-web.js
@@ -1,136 +1,136 @@
 const pipeline = (env) => {
-    const taskCheckFeed = require('../tasks/check-feed')
-    const taskDequeueBook = require('../tasks/dequeue-book')
-    const taskFetchBookGroup = require('../tasks/fetch-book-group')
-    const taskAssembleBookGroup = require('../tasks/assemble-book-group')
-    const taskLinkSingle = require('../tasks/link-single')
-    const taskAssembleBookMetaGroup = require('../tasks/assemble-book-metadata-group')
-    const taskBakeBookGroup = require('../tasks/bake-book-group')
-    const taskBakeBookMetaGroup = require('../tasks/bake-book-metadata-group')
-    const taskChecksumSingle = require('../tasks/checksum-single')
-    const taskDisassembleSingle = require('../tasks/disassemble-single')
-    const taskJsonifySingle = require('../tasks/jsonify-single')
-    const taskValidateXhtml = require('../tasks/validate-xhtml')
-    const taskUploadSingle = require('../tasks/upload-single')
-    const taskReportStateComplete = require('../tasks/report-state-complete')
+  const taskCheckFeed = require('../tasks/check-feed')
+  const taskDequeueBook = require('../tasks/dequeue-book')
+  const taskFetchBookGroup = require('../tasks/fetch-book-group')
+  const taskAssembleBookGroup = require('../tasks/assemble-book-group')
+  const taskLinkSingle = require('../tasks/link-single')
+  const taskAssembleBookMetaGroup = require('../tasks/assemble-book-metadata-group')
+  const taskBakeBookGroup = require('../tasks/bake-book-group')
+  const taskBakeBookMetaGroup = require('../tasks/bake-book-metadata-group')
+  const taskChecksumSingle = require('../tasks/checksum-single')
+  const taskDisassembleSingle = require('../tasks/disassemble-single')
+  const taskJsonifySingle = require('../tasks/jsonify-single')
+  const taskValidateXhtml = require('../tasks/validate-xhtml')
+  const taskUploadSingle = require('../tasks/upload-single')
+  const taskReportStateComplete = require('../tasks/report-state-complete')
 
-    const awsAccessKeyId = env.S3_ACCESS_KEY_ID
-    const awsSecretAccessKey = env.S3_SECRET_ACCESS_KEY
-    const codeVersionFromTag = env.IMAGE_TAG || 'version-unknown'
-    const githubSecretCreds = env.GH_SECRET_CREDS
-    const queueFilename = `${codeVersionFromTag}.${env.DIST_GIT_QUEUE_FILENAME}`
-    const queueStatePrefix = 'git-dist'
-    const lockedTag = env.IMAGE_TAG || 'trunk'
+  const awsAccessKeyId = env.S3_ACCESS_KEY_ID
+  const awsSecretAccessKey = env.S3_SECRET_ACCESS_KEY
+  const codeVersionFromTag = env.IMAGE_TAG || 'version-unknown'
+  const githubSecretCreds = env.GH_SECRET_CREDS
+  const queueFilename = `${codeVersionFromTag}.${env.DIST_GIT_QUEUE_FILENAME}`
+  const queueStatePrefix = 'git-dist'
+  const lockedTag = env.IMAGE_TAG || 'trunk'
 
-    const imageOverrides = {
-        tag: lockedTag,
-        ...env.dockerCredentials
+  const imageOverrides = {
+    tag: lockedTag,
+    ...env.dockerCredentials
+  }
+
+  const resources = [
+    {
+      name: 'cnx-recipes-output',
+      type: 'docker-image',
+      source: {
+        repository: 'openstax/cnx-recipes-output',
+        ...imageOverrides
+      }
+    },
+    {
+      name: 's3-queue',
+      type: 's3',
+      source: {
+        bucket: env.DIST_QUEUE_STATE_S3_BUCKET,
+        versioned_file: queueFilename,
+        initial_version: 'initializing',
+        access_key_id: awsAccessKeyId,
+        secret_access_key: awsSecretAccessKey
+      }
+    },
+    {
+      name: 'ticker',
+      type: 'time',
+      source: {
+        interval: env.PIPELINE_TICK_INTERVAL
+      }
     }
+  ]
 
-    const resources = [
-        {
-            name: 'cnx-recipes-output',
-            type: 'docker-image',
-            source: {
-                repository: 'openstax/cnx-recipes-output',
-                ...imageOverrides
-            }
-        },
-        {
-            name: 's3-queue',
-            type: 's3',
-            source: {
-                bucket: env.DIST_QUEUE_STATE_S3_BUCKET,
-                versioned_file: queueFilename,
-                initial_version: 'initializing',
-                access_key_id: awsAccessKeyId,
-                secret_access_key: awsSecretAccessKey
-            }
-        },
-        {
-            name: 'ticker',
-            type: 'time',
-            source: {
-                interval: env.PIPELINE_TICK_INTERVAL
-            }
-        }
+  const feederJob = {
+    name: 'feeder',
+    plan: [
+      { get: 'ticker', trigger: true },
+      taskCheckFeed({
+        awsAccessKeyId: awsAccessKeyId,
+        awsSecretAccessKey: awsSecretAccessKey,
+        feedFileUrl: env.DIST_GIT_FEED_FILE_URL,
+        queueStateBucket: env.DIST_QUEUE_STATE_S3_BUCKET,
+        queueFilename: queueFilename,
+        codeVersion: codeVersionFromTag,
+        maxBooksPerRun: env.MAX_BOOKS_PER_TICK,
+        statePrefix: queueStatePrefix,
+        image: imageOverrides
+      })
     ]
+  }
 
-    const feederJob = {
-        name: 'feeder',
-        plan: [
-            { get: 'ticker', trigger: true },
-            taskCheckFeed({
-                awsAccessKeyId: awsAccessKeyId,
-                awsSecretAccessKey: awsSecretAccessKey,
-                feedFileUrl: env.DIST_GIT_FEED_FILE_URL,
-                queueStateBucket: env.DIST_QUEUE_STATE_S3_BUCKET,
-                queueFilename: queueFilename,
-                codeVersion: codeVersionFromTag,
-                maxBooksPerRun: env.MAX_BOOKS_PER_TICK,
-                statePrefix: queueStatePrefix,
-                image: imageOverrides
-            })
-        ]
-    }
+  const gitWebHostJob = {
+    name: 'git-webhosting-job',
+    max_in_flight: 5,
+    plan: [
+      { get: 's3-queue', trigger: true, version: 'every' },
+      { get: 'cnx-recipes-output' },
+      taskDequeueBook({
+        queueFilename: queueFilename,
+        image: imageOverrides
+      }),
+      taskFetchBookGroup({
+        githubSecretCreds: githubSecretCreds,
+        image: imageOverrides
+      }),
+      taskAssembleBookGroup({ image: imageOverrides }),
+      taskAssembleBookMetaGroup({ image: imageOverrides }),
+      taskBakeBookGroup({ image: imageOverrides }),
+      taskBakeBookMetaGroup({ image: imageOverrides }),
+      taskLinkSingle({ image: imageOverrides }),
+      taskChecksumSingle({ image: imageOverrides }),
+      taskDisassembleSingle({ image: imageOverrides }),
+      taskJsonifySingle({
+        image: imageOverrides
+      }),
+      taskValidateXhtml({
+        image: imageOverrides,
+        inputSource: 'jsonified-single',
+        inputPath: '/*@*.xhtml',
+        validationNames: ['duplicate-id', 'broken-link']
+      }),
+      taskUploadSingle({
+        distBucket: env.DIST_S3_BUCKET,
+        distBucketPath: 'apps/archive/',
+        queueStateBucket: env.DIST_QUEUE_STATE_S3_BUCKET,
+        awsAccessKeyId: awsAccessKeyId,
+        awsSecretAccessKey: awsSecretAccessKey,
+        codeVersion: codeVersionFromTag,
+        statePrefix: queueStatePrefix,
+        image: imageOverrides
+      }),
+      taskReportStateComplete({
+        image: imageOverrides,
+        awsAccessKeyId: awsAccessKeyId,
+        awsSecretAccessKey: awsSecretAccessKey,
+        queueStateBucket: env.DIST_QUEUE_STATE_S3_BUCKET,
+        codeVersion: codeVersionFromTag,
+        statePrefix: queueStatePrefix
+      })
+    ]
+  }
 
-    const gitWebHostJob = {
-        name: 'git-webhosting-job',
-        max_in_flight: 5,
-        plan: [
-            { get: 's3-queue', trigger: true, version: 'every' },
-            { get: 'cnx-recipes-output' },
-            taskDequeueBook({
-                queueFilename: queueFilename,
-                image: imageOverrides
-            }),
-            taskFetchBookGroup({
-                githubSecretCreds: githubSecretCreds,
-                image: imageOverrides
-            }),
-            taskAssembleBookGroup({ image: imageOverrides }),
-            taskAssembleBookMetaGroup({ image: imageOverrides }),
-            taskBakeBookGroup({ image: imageOverrides }),
-            taskBakeBookMetaGroup({ image: imageOverrides }),
-            taskLinkSingle({ image: imageOverrides }),
-            taskChecksumSingle({ image: imageOverrides }),
-            taskDisassembleSingle({ image: imageOverrides }),
-            taskJsonifySingle({
-                image: imageOverrides
-            }),
-            taskValidateXhtml({
-                image: imageOverrides,
-                inputSource: 'jsonified-single',
-                inputPath: '/*@*.xhtml',
-                validationNames: ['duplicate-id', 'broken-link']
-            }),
-            taskUploadSingle({
-                distBucket: env.DIST_S3_BUCKET,
-                distBucketPath: 'apps/archive/',
-                queueStateBucket: env.DIST_QUEUE_STATE_S3_BUCKET,
-                awsAccessKeyId: awsAccessKeyId,
-                awsSecretAccessKey: awsSecretAccessKey,
-                codeVersion: codeVersionFromTag,
-                statePrefix: queueStatePrefix,
-                image: imageOverrides
-            }),
-            taskReportStateComplete({
-                image: imageOverrides,
-                awsAccessKeyId: awsAccessKeyId,
-                awsSecretAccessKey: awsSecretAccessKey,
-                queueStateBucket: env.DIST_QUEUE_STATE_S3_BUCKET,
-                codeVersion: codeVersionFromTag,
-                statePrefix: queueStatePrefix
-            })
-        ]
+  return {
+    config: {
+      resources: resources,
+      jobs: [feederJob, gitWebHostJob]
     }
-
-    return {
-        config: {
-            resources: resources,
-            jobs: [feederJob, gitWebHostJob]
-        }
-    }
+  }
 }
 
 module.exports = pipeline
