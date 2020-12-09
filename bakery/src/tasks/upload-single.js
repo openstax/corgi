1538f98b9ccf408bd14655c9c9939d48b98a6271
Brenda Nguyen <bn16@rice.edu>
2020-12-09 12:51:27 -0800

    maintain consistent git task format

--- bakery/src/tasks/upload-single.js
+++ bakery/src/tasks/upload-single.js
@@ -14,6 +14,12 @@ const task = (taskArgs) => {
     throw Error('distBucketPath must represent some directory-like path in s3')
   }
   const distBucketPrefix = `${distBucketPath}${codeVersion}`
+  const bookInput = 'book'
+  const jsonifiedInput = 'jsonified-single'
+  const checksumInput = 'checksum-single'
+  const resourceInput = 'fetched-book-group-resources'
+  const uploadOutput = 'upload-single'
+  const shellScript = fs.readFileSync(path.resolve(__dirname, '../scripts/upload_single.sh'), { encoding: 'utf-8' })
 
   return {
     task: 'upload single',
@@ -28,40 +34,26 @@ const task = (taskArgs) => {
         AWS_SECRET_ACCESS_KEY: `${awsSecretAccessKey}`
       },
       inputs: [
-        { name: 'book' },
-        { name: 'jsonified-single' },
-        { name: 'checksum-single' },
-        { name: 'fetched-book-group-resources' }
+        { name: bookInput },
+        { name: jsonifiedInput },
+        { name: checksumInput },
+        { name: resourceInput }
       ],
+      outputs: [
+        { name: uploadOutput }
+      ],
+      params: {
+        BUCKET_PREFIX: distBucketPrefix,
+        BOOK_INPUT: bookInput,
+        JSONIFIED_INPUT: jsonifiedInput,
+        CHECKSUM_INPUT: checksumInput,
+        UPLOAD_OUTPUT: uploadOutput
+      },
       run: {
         path: '/bin/bash',
         args: [
           '-cxe',
-          dedent`
-          exec 2> >(tee upload-single/stderr >&2)
-          target_dir="upload-single/contents"
-          mkdir -p "$target_dir"
-          book_dir="jsonified-single"
-          resources_dir="checksum-single/resources"
-          book_uuid=$(cat book/uuid)
-          book_version=$(cat book/version)
-          book_slug=$(cat book/slug)
-          for jsonfile in "$book_dir/"*@*.json; do cp "$jsonfile" "$target_dir/$(basename $jsonfile)"; done;
-          for xhtmlfile in "$book_dir/"*@*.xhtml; do cp "$xhtmlfile" "$target_dir/$(basename $xhtmlfile)"; done;
-          aws s3 cp --recursive "$target_dir" "s3://${distBucket}/${distBucketPrefix}/contents"
-          copy-resources-s3 "$resources_dir" "${distBucket}" "${distBucketPrefix}/resources"
-
-          #######################################
-          # UPLOAD BOOK LEVEL FILES LAST
-          # so that if an error is encountered
-          # on prior upload steps, those files
-          # will not be found by watchers
-          #######################################
-          toc_s3_link_json="s3://${distBucket}/${distBucketPrefix}/contents/$book_uuid@$book_version.json"
-          toc_s3_link_xhtml="s3://${distBucket}/${distBucketPrefix}/contents/$book_uuid@$book_version.xhtml"
-          aws s3 cp "$book_dir/$book_slug.toc.json" "$toc_s3_link_json"
-          aws s3 cp "$book_dir/$book_slug.toc.xhtml" "$toc_s3_link_xhtml"
-        `
+          shellScript
         ]
       }
     }
