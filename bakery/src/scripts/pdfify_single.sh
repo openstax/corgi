691f8a94b4b10b97158f4a9c94e2ee526c08d094
Ripal Nathuji <rnathuji@neoteklabs.com>
2020-12-17 12:58:27 -0600

    Copy style file from input during pdfify-single
    This fixes a regression from an inadvertent removal of this line of
    code.

--- bakery/src/scripts/pdfify_single.sh
+++ bakery/src/scripts/pdfify_single.sh
@@ -2,5 +2,7 @@
 
 exec 2> >(tee "${ARTIFACTS_OUTPUT}"/stderr >&2)
 
+cp "${STYLE_INPUT}"/* "${MATHIFIED_INPUT}"
+
 echo -n "https://${BUCKET_NAME}.s3.amazonaws.com/$(cat "${BOOK_INPUT}"/pdf_filename)" > "${ARTIFACTS_OUTPUT}/pdf_url"
 prince -v --output="${ARTIFACTS_OUTPUT}/$(cat "${BOOK_INPUT}"/pdf_filename)" "${MATHIFIED_INPUT}/$(cat "${BOOK_INPUT}"/slug).mathified.xhtml"
