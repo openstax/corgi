d0e2b5cac0c1a03ade42cce03fb6a995de5aaf1a
therealmarv <1050582+therealmarv@users.noreply.github.com>
2020-12-21 18:47:44 +0200

    fix tests

--- bakery/src/tests/test_bakery_scripts.py
+++ bakery/src/tests/test_bakery_scripts.py
@@ -1414,19 +1414,22 @@ def test_gdocify_book(tmp_path, mocker):
 
     # Test fix_jpeg_colorspace
     with TemporaryDirectory() as temp_dir:
+
         # copy test JPEGs into a temporal dir
         copy_tree(TEST_JPEG_DIR, temp_dir)
 
-        rgb = 'rgb.jpg'
-        rgb_broken = 'rgb_broken.jpg'
-        cmyk = 'cmyk.jpg'
-        cmyk_broken = 'cmyk_broken.jpg'
-        greyscale = 'greyscale.jpg'
-        greyscale_broken = 'greyscale_broken.jpg'
-        png = 'original_public_domain.png'
+        rf = '../resources/'
+        rgb = rf + 'rgb.jpg'
+        rgb_broken = rf + 'rgb_broken.jpg'
+        cmyk = rf + 'cmyk.jpg'
+        cmyk_broken = rf + 'cmyk_broken.jpg'
+        greyscale = rf + 'greyscale.jpg'
+        greyscale_broken = rf + 'greyscale_broken.jpg'
+        png = rf + 'original_public_domain.png'
 
         old_dir = os.getcwd()
-        os.chdir(temp_dir)
+        # to get the ..resources images change directory to this start point
+        os.chdir(os.path.join(temp_dir, 'startpoint'))
 
         # convert to RGB
         xhtml = """
