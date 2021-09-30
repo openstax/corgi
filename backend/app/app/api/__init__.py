5969939dda9b58785b6686f9a21c875e8bdb12c1
m1yag1 <mike.arbelaez@rice.edu>
2021-09-30 18:02:36 -0500

    Register the api endpoint with the application router

--- backend/app/app/api/__init__.py
+++ backend/app/app/api/__init__.py
@@ -3,7 +3,8 @@ from fastapi import APIRouter
 from app.api.endpoints import (content_servers,
                                jobs,
                                ping,
-                               status)
+                               status,
+                               version)
 
 api_router = APIRouter()
 api_router.include_router(ping.router, prefix="/ping", tags=["ping"])
@@ -11,3 +12,4 @@ api_router.include_router(jobs.router, prefix="/jobs", tags=["jobs"])
 api_router.include_router(status.router, prefix="/status", tags=["status"])
 api_router.include_router(content_servers.router, prefix="/content-servers",
                           tags=["content_servers"])
+api_router.include_router(version.router, prefix="/version", tags=["version"])
