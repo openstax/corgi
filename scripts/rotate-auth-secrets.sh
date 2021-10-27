2628e24dffd15ff294ba1f4ec923f327066fa30a
m1yag1 <mike.arbelaez@rice.edu>
2021-10-27 12:17:25 -0500

    Updated rotate-auth-secrets.sh to use corgi and not cops

--- scripts/rotate-auth-secrets.sh
+++ scripts/rotate-auth-secrets.sh
@@ -1,21 +1,21 @@
 #!/usr/bin/env bash
 set -e
 
-[[ -z "$COPS_HTACCESS_FILE" ]] && echo "Error: COPS_HTACCESS_FILE not set" && exit 1
-[[ -s "$COPS_HTACCESS_FILE" ]] || ( echo "Error: COPS_HTACCESS_FILE does not exist or is empty" && exit 1 )
+[[ -z "$CORGI_HTACCESS_FILE" ]] && echo "Error: CORGI_HTACCESS_FILE not set" && exit 1
+[[ -s "$CORGI_HTACCESS_FILE" ]] || ( echo "Error: CORGI_HTACCESS_FILE does not exist or is empty" && exit 1 )
 
-cops_test_proxy_target="corgi_stag_proxy"
+corgi_test_proxy_target="corgi_stag_proxy"
 auth_secret_name="basic-auth-users"
 auth_secret_name_temp="${auth_secret_name}-temp"
 
-docker secret create $auth_secret_name_temp "$COPS_HTACCESS_FILE"
+docker secret create $auth_secret_name_temp "$CORGI_HTACCESS_FILE"
 set +e
 
 revert_any_temp() {
   echo "Cleaning up (conflicting target errors are ok)..."
-  for cops_proxy_target in corgi_stag_proxy corgi_prod_proxy cops_prod_proxy; do
-    docker service update --secret-rm $auth_secret_name_temp $cops_proxy_target
-    docker service update --secret-add $auth_secret_name $cops_proxy_target
+  for corgi_proxy_target in corgi_stag_proxy corgi_prod_proxy; do
+    docker service update --secret-rm $auth_secret_name_temp $corgi_proxy_target
+    docker service update --secret-add $auth_secret_name $corgi_proxy_target
   done
   docker secret rm $auth_secret_name_temp
   echo "Done."
@@ -24,8 +24,8 @@ trap revert_any_temp ERR
 trap revert_any_temp EXIT
 
 echo "Creating a temp rotation on staging for acceptance..."
-docker service update --secret-rm $auth_secret_name $cops_test_proxy_target
-docker service update --secret-add "source=${auth_secret_name_temp},target=${auth_secret_name}" $cops_test_proxy_target
+docker service update --secret-rm $auth_secret_name $corgi_test_proxy_target
+docker service update --secret-add "source=${auth_secret_name_temp},target=${auth_secret_name}" $corgi_test_proxy_target
 echo "Temp rotation in place on staging. Try it: https://corgi-staging.openstax.org"
 read -r -n 1 -p "(R)evert | (a)ccept: " accept_char
 
@@ -40,15 +40,15 @@ if [[ "$accept_char" = "a" ]] || [[ "$accept_char" = "A" ]]; then
   echo -n "1 " && sleep 1
   echo
 
-  docker service update --secret-rm $auth_secret_name_temp $cops_test_proxy_target
+  docker service update --secret-rm $auth_secret_name_temp $corgi_test_proxy_target
 
-  for cops_proxy_target in corgi_stag_proxy corgi_prod_proxy cops_prod_proxy; do
-    docker service update --secret-rm $auth_secret_name $cops_proxy_target
+  for corgi_proxy_target in corgi_stag_proxy corgi_prod_proxy; do
+    docker service update --secret-rm $auth_secret_name $corgi_proxy_target
   done
   docker secret rm $auth_secret_name
-  docker secret create $auth_secret_name "$COPS_HTACCESS_FILE"
-  for cops_proxy_target in corgi_stag_proxy corgi_prod_proxy cops_prod_proxy; do
-    docker service update --secret-add $auth_secret_name $cops_proxy_target
+  docker secret create $auth_secret_name "$CORGI_HTACCESS_FILE"
+  for corgi_proxy_target in corgi_stag_proxy corgi_prod_proxy; do
+    docker service update --secret-add $auth_secret_name $corgi_proxy_target
   done
 else
   echo "Reverting..."
