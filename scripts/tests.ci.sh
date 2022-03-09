ce1f7b996c2b5177b2f23a6de4c32c4fbf47dfce
m1yag1 <mike.arbelaez@rice.edu>
2022-03-09 10:10:45 -0600

    Add docker-compose.dev.images to /scripts/tests.ci.sh and restart backend container

--- scripts/tests.ci.sh
+++ scripts/tests.ci.sh
@@ -16,6 +16,7 @@ docker-compose \
     -f docker-compose.shared.base-images.yml \
     -f docker-compose.shared.depends.yml \
     -f docker-compose.shared.env.yml \
+    -f docker-compose.dev.images.yml \
     -f docker-compose.deploy.build.yml \
     -f docker-compose.tests.yml \
     config > docker-stack.yml
@@ -26,7 +27,8 @@ docker-compose -f docker-stack.yml up -d
 docker-compose -f docker-stack.yml exec backend-tests wait-for-it -t 10 db:5432
 docker-compose -f docker-stack.yml exec db psql -h db -d postgres -U postgres -c "DROP DATABASE IF EXISTS tests"
 docker-compose -f docker-stack.yml exec db psql -h db -d postgres -U postgres -c "CREATE DATABASE tests ENCODING 'UTF8'"
+docker-compose -f docker-stack.yml restart backend
 docker-compose -f docker-stack.yml exec backend-tests wait-for-it -t 10 backend:80
-docker-compose -f docker-stack.yml exec -T backend-tests pytest ./tests -vvv -m "ui or integration" --junitxml="${TEST_RESULTS}" --driver Chrome --base-url http://frontend --headless
+docker-compose -f docker-stack.yml exec -T backend-tests pytest ./tests -vvv -m "ui or integration" --junitxml="${TEST_RESULTS}" --driver Chrome --base-url http://frontend 
 # Comment this line out to leave the stack running. Useful for test development.
 docker-compose -f docker-stack.yml down -v --remove-orphans
