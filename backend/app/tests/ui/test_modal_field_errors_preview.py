60dcc12b7d67a9cb5cf2ef9d7ed2ac2e5e9c905d
Otto Mehes <om9@rice.edu>
2022-03-23 15:20:07 -0500

    adding test_modal_field_errors_preview to the test framework

--- backend/app/tests/ui/test_modal_field_errors_preview.py
+++ backend/app/tests/ui/test_modal_field_errors_preview.py
@@ -1,71 +1,83 @@
 import pytest
-
-from pages.home import Home
-
 from pytest_testrail.plugin import pytestrail
+
+from pages.home import HomeCorgi
 
 
 @pytestrail.case("C624696")
-@pytest.mark.smoke
 @pytest.mark.ui
 @pytest.mark.nondestructive
-def test_invalid_colid_error_preview(selenium, base_url):
-    # GIVEN: Selenium driver and the base url
+def test_invalid_colid_error_preview(chrome_page, corgi_base_url):
+    # GIVEN: Playwright, chromium and the corgi_base_url
 
     # WHEN: The Home page is fully loaded
-    home = Home(selenium, base_url).open()
+    chrome_page.goto(corgi_base_url)
+    home = HomeCorgi(chrome_page)
 
-    # AND: The create a new job button is clicked
-    modal = home.click_create_new_job_button()
+    # THEN: The create a new job button is clicked
+    home.click_create_new_job_button()
 
     # AND: Clicks the Web Preview button
-    modal.click_web_preview_radio_button()
+    home.click_web_preview_radio_button()
 
     # AND: Incorrect collection id is typed into the collection id field
-    modal.fill_collection_id_field("1col11229")
+    home.fill_collection_id_field("1col11229")
 
     # AND: Create button is clicked
-    modal.click_create_button()
-
-    split_col_id_incorrect = modal.collection_id_incorrect_field_error.text.splitlines()
-    text_col_id_incorrect = split_col_id_incorrect[1]
+    home.click_create_button()
 
     # THEN: Correct error message appears in collection id field
-    assert "A valid collection ID is required, e.g. col12345" == text_col_id_incorrect
+    assert (
+        "A valid collection ID is required, e.g. col12345"
+        == home.collection_id_field_texts.text_content()
+    )
 
-    split_style = modal.style_field_error.text.splitlines()
-    text_style = split_style[1]
-    assert "Style is required" == text_style
+    assert "Style is required" == home.style_field_texts.text_content()
 
-    split_server = modal.content_server_field_error.text.splitlines()
-    text_server = split_server[1]
-    assert "Please select a server" == text_server
+    assert "Please select a server" == home.content_server_field_texts.text_content()
 
-    # THEN: The modal does not close and remains open
+    # THEN: The home does not close and remains open
     assert home.create_job_modal_is_open
 
-    # WHEN: modal is open and collection id has incorrect colid/slug
+
+@pytestrail.case("C646766")
+@pytest.mark.ui
+@pytest.mark.nondestructive
+def test_invalid_colid_error_git_preview(chrome_page, corgi_base_url):
+    # GIVEN: Playwright, chromium and the corgi_base_url
+
+    # WHEN: The Home page is fully loaded
+    chrome_page.goto(corgi_base_url)
+    home = HomeCorgi(chrome_page)
+
+    # THEN: The create a new job button is clicked
+    home.click_create_new_job_button()
+
     # AND: Web preview (git) button is clicked
-    modal.click_web_preview_git_radio_button()
+    home.click_web_preview_git_radio_button()
 
-    # AND: Create button is clicked when data fields are empty and collection ID field has incorrect colid
-    modal.click_create_button()
+    # AND: Create button is clicked when data fields are empty
+    home.click_create_button()
 
-    split_col_id_slug_incorrect = (
-        modal.collection_id_slug_incorrect_field_error.text.splitlines()
+    # THEN: Correct error message appears in collection id and style field
+    assert "Repo and slug are required" == home.collection_id_field_texts.text_content()
+
+    assert "Style is required" == home.style_field_texts.text_content()
+
+    # THEN: No error message appears for Content Server as it is disabled for web preview git
+    assert (
+        "Please select a server" not in home.content_server_field_texts.text_content()
     )
-    text_col_id_slug_incorrect = split_col_id_slug_incorrect[1]
+
+    # AND: Collection ID field has incorrect colid
+    home.fill_collection_id_field(
+        "osbooks_fizyka_bundle1/fizyka=dla-szkół-wyższych-tom-1"
+    )
 
     # THEN: Correct error message appears in collection id and style field
     assert (
         "A valid repo and slug name is required, e.g. repo-name/slug-name"
-        == text_col_id_slug_incorrect
+        == home.collection_id_field_texts.text_content()
     )
 
-    split_style = modal.style_field_error.text.splitlines()
-    text_style = split_style[1]
-    assert "Style is required" == text_style
-
-    # THEN: No error message appears for Content Server as it is disabled for web preview git
-    split_server = modal.content_server_field_error.text.splitlines()
-    assert "Please select a server" not in split_server
+    assert "Style is required" == home.style_field_texts.text_content()
