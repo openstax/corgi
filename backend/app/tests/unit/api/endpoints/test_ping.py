c8e5334e59d0eb0709d894387af0b808b5dc5d2b
m1yag1 <mike.arbelaez@rice.edu>
2022-03-24 14:35:36 -0500

    added marker to test_ping_get_request

--- backend/app/tests/unit/api/endpoints/test_ping.py
+++ backend/app/tests/unit/api/endpoints/test_ping.py
@@ -1,5 +1,9 @@
+import pytest
+
 ENDPOINT = "ping"
 
+@pytest.mark.unit
+@pytest.mark.nondestructive
 def test_ping_get_request(testclient):
     # GIVEN: a test client and a URL
     url = f"/api/{ENDPOINT}"
