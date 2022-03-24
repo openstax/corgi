f7ffa3bdb13581619771a874fb8330ad5820332b
m1yag1 <mike.arbelaez@rice.edu>
2022-03-24 14:37:21 -0500

    Remove convert_unicode from create_engine as it's deprecated

--- backend/app/app/db/session.py
+++ backend/app/app/db/session.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import scoped_session, sessionmaker
 
 from app.core import config
 
-engine = create_engine(config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True, convert_unicode=True)
+engine = create_engine(config.SQLALCHEMY_DATABASE_URI, pool_pre_ping=True)
 
 session_factory = sessionmaker(bind=engine)
 
