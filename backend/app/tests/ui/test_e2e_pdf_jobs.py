2f1fa7f79f4a25fd60ceb9fea3cadd9aca8eac45
Otto Mehes <om9@rice.edu>
2022-03-28 09:46:36 -0500

    adding test_e2e_pdf_jobs.py to test framework

--- backend/app/tests/ui/test_e2e_pdf_jobs.py
+++ backend/app/tests/ui/test_e2e_pdf_jobs.py
@@ -1,32 +1,32 @@
 import pytest
-
-from pages.home import Home
-
 from pytest_testrail.plugin import pytestrail
+
+from pages.home import HomeCorgi
 
 
 @pytestrail.case("C606121")
-@pytest.mark.smoke
 @pytest.mark.ui
 @pytest.mark.nondestructive
-def test_e2e_pdf_jobs(selenium, base_url):
-    # GIVEN: Selenium driver and the base url
+def test_e2e_pdf_jobs(chrome_page, corgi_base_url):
+    # GIVEN: Playwright, chromium and the corgi_base_url
 
     # WHEN: The Home page is fully loaded
-    home = Home(selenium, base_url).open()
+    chrome_page.goto(corgi_base_url)
+    home = HomeCorgi(chrome_page)
 
-    # AND: The 'create a new job' button is clicked
-    modal = home.click_create_new_job_button()
+    # THEN: The create a new job button is clicked
+    home.click_create_new_job_button()
 
     # AND: PDF radio button is automatically selected and correct data are typed into the input fields
-    modal.fill_collection_id_field("col11992")
-    modal.fill_version_field("1.9")
-    modal.fill_style_field("astronomy")
-    modal.fill_server_field("qa")
+    home.fill_collection_id_field("col11992")
+    home.fill_version_field("1.9")
+    home.fill_style_field("astronomy")
+    home.click_content_server()
+    home.click_content_server_dropdown("qa")
 
     # AND: Create button is clicked
-    modal.click_create_button()
+    home.click_create_button()
 
     # THEN: The modal closes and job is queued
-    assert home.is_create_new_job_button_displayed
-    assert modal.status_message.text == "queued"
+    assert home.create_new_job_button_is_visible
+    assert "queued" in home.status_message.text_content()
