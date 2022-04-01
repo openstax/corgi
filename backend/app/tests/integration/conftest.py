23e9a99605a257413257eb6a409ca3b0f97260ba
m1yag1 <mike.arbelaez@rice.edu>
2022-04-01 18:34:54 -0500

    Added fixtures for setting corgi environment variables

--- backend/app/tests/integration/conftest.py
+++ backend/app/tests/integration/conftest.py
@@ -3,10 +3,6 @@ import os
 import pytest
 
 
-@pytest.fixture(scope="module")
-def api_url(base_url):
-    return f"{base_url}/api"
-
 
 # Get values we need to setup the environment from the command line or through env vars
 def pytest_addoption(parser):
@@ -17,19 +13,64 @@ def pytest_addoption(parser):
         help="The name of the stack, used in the version endpoint",
     )
     parser.addoption(
-            "--revision",
-            default=os.getenv("REVISION", "dev"),
-            help="The sha/revision of CORGI deployed",
-        )
+        "--revision",
+        default=os.getenv("REVISION", "dev"),
+        help="The sha/revision of CORGI deployed",
+    )
     parser.addoption(
-            "--deployed_at",
-            default=os.getenv("DEPLOYED_AT", "dev"),
-            help="The timestamp CORGI was deployed",
-        )
+        "--deployed_at",
+        default=os.getenv("DEPLOYED_AT", "dev"),
+        help="The timestamp CORGI was deployed",
+    )
     parser.addoption(
-            "--tag",
-            default=os.getenv("TAG", "dev"),
-            help="The tag used on the images",
-        )
+        "--tag",
+        default=os.getenv("TAG", "dev"),
+        help="The tag used on the images",
+    )
     
-    
+@pytest.fixture(scope="session")
+def api_url(base_url):
+    return f"{base_url}/api"
+
+
+@pytest.fixture(scope="session")
+def tag(request):
+    """Return the tag"""
+    config = request.config
+    tag = config.getoption("tag")
+    if tag is not None:
+        return tag
+    else:
+        return "dev"
+
+
+@pytest.fixture(scope="session")
+def stack_name(request):
+    """Return the tag"""
+    config = request.config
+    stack_name = config.getoption("stack_name")
+    if stack_name is not None:
+        return stack_name
+    else:
+        return "dev"
+
+
+@pytest.fixture(scope="session")
+def revision(request):
+    """Return the tag"""
+    config = request.config
+    stack_name = config.getoption("revision")
+    if revision is not None:
+        return revision
+    else:
+        return "dev"
+
+@pytest.fixture(scope="session")
+def deployed_at(request):
+    """Return the deployed_at timestamp"""
+    config = request.config
+    deployed_at = config.getoption("deployed_at")
+    if deployed_at is not None:
+        return deployed_at
+    else:
+        return "dev"
