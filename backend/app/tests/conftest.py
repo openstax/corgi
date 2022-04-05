1d52e6b2f61f69ac3d1ecdd3cc52764cca8b5cea
Otto Mehes <om9@rice.edu>
2022-04-05 13:21:37 -0500

    removing selenium parts from testframework

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -1,30 +1,5 @@
-import os
-
 # Import fixtures
 pytest_plugins = "tests.ui.fixtures.ui"
-
-
-# These 3 adoptions can be removed once all tests are in playwright
-def pytest_addoption(parser):
-    group = parser.getgroup("selenium", "selenium")
-    group.addoption(
-        "--disable-dev-shm-usage",
-        action="store_true",
-        default=os.getenv("DISABLE_DEV_SHM_USAGE", False),
-        help="disable chrome's usage of /dev/shm. (used by Travis)",
-    )
-    group.addoption(
-        "--headless",
-        action="store_true",
-        default=os.getenv("HEADLESS", False),
-        help="enable headless mode for chrome. So chrome does not interrupt you.",
-    )
-    group.addoption(
-        "--no-sandbox",
-        action="store_true",
-        default=os.getenv("NO_SANDBOX", False),
-        help="disable chrome's sandbox. (used by Travis)",
-    )
 
 
 def get_custom_markers():
