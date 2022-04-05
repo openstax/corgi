0d6db86df9b23c11c8e559e27d955df69eda14c5
m1yag1 <mike.arbelaez@rice.edu>
2022-04-05 10:58:50 -0500

    Update test and fixtures for PR environment

--- backend/app/tests/integration/api/endpoints/test_version.py
+++ backend/app/tests/integration/api/endpoints/test_version.py
@@ -6,16 +6,15 @@ ENDPOINT = "version"
 
 @pytest.mark.integration
 @pytest.mark.nondestructive
-def test_version_get_request(api_url, stack_name, revision, tag, deployed_at):
+def test_version_get_request(api_url, stack_name, tag):
     # GIVEN: An api url to the version endpoint
     url = f"{api_url}/{ENDPOINT}"
 
     # WHEN: A GET request is made to the url
     response = requests.get(url)
 
+    version_data = response.json()
+
     # THEN: A proper response is returned
-    assert response.json() == {"stack_name": stack_name,
-                               "revision": revision,
-                               "tag": tag,
-                               "deployed_at": deployed_at
-                               }
+    assert version_data["stack_name"] == stack_name
+    assert version_data["tag"] == tag
--- backend/app/tests/integration/conftest.py
+++ backend/app/tests/integration/conftest.py
@@ -13,21 +13,12 @@ def pytest_addoption(parser):
         help="The name of the stack, used in the version endpoint",
     )
     parser.addoption(
-        "--revision",
-        default=os.getenv("REVISION", "dev"),
-        help="The sha/revision of CORGI deployed",
-    )
-    parser.addoption(
-        "--deployed_at",
-        default=os.getenv("DEPLOYED_AT", "dev"),
-        help="The timestamp CORGI was deployed",
-    )
-    parser.addoption(
         "--tag",
         default=os.getenv("TAG", "dev"),
         help="The tag used on the images",
     )
-    
+
+
 @pytest.fixture(scope="session")
 def api_url(base_url):
     return f"{base_url}/api"
@@ -46,31 +37,10 @@ def tag(request):
 
 @pytest.fixture(scope="session")
 def stack_name(request):
-    """Return the tag"""
+    """Return the name of the stack"""
     config = request.config
     stack_name = config.getoption("stack_name")
     if stack_name is not None:
         return stack_name
-    else:
-        return "dev"
-
-
-@pytest.fixture(scope="session")
-def revision(request):
-    """Return the tag"""
-    config = request.config
-    stack_name = config.getoption("revision")
-    if revision is not None:
-        return revision
-    else:
-        return "dev"
-
-@pytest.fixture(scope="session")
-def deployed_at(request):
-    """Return the deployed_at timestamp"""
-    config = request.config
-    deployed_at = config.getoption("deployed_at")
-    if deployed_at is not None:
-        return deployed_at
     else:
         return "dev"
