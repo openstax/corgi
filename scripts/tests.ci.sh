cb64a4215ac70675f219ba8ee7298bf043c24485
m1yag1 <mike.arbelaez@rice.edu>
2022-04-07 14:42:24 -0500

    Update tests.ci.sh to use new composite stack file

--- scripts/tests.ci.sh
+++ scripts/tests.ci.sh
@@ -7,18 +7,14 @@ TEST_RESULTS=${TEST_RESULTS-./junit.xml}
 
 echo "Test results will be saved in: ${TEST_RESULTS}"
 
-DOMAIN=backend \
-REVISION=dev \
-TAG=dev \
-STACK_NAME=dev \
-DEPLOYED_AT=20210101.111111 \
+export DOMAIN=backend
+export TAG=dev
+export STACK_NAME=corgi_dev
+export REVISION=dev
+export BASE_URL=http://backend
+
 docker-compose \
-    -f docker-compose.shared.base-images.yml \
-    -f docker-compose.shared.depends.yml \
-    -f docker-compose.shared.env.yml \
-    -f docker-compose.dev.images.yml \
-    -f docker-compose.deploy.build.yml \
-    -f docker-compose.tests.yml \
+    -f docker-compose.stack.ci.yml \
     config > docker-stack.yml
 
 docker-compose -f docker-stack.yml build
@@ -29,6 +25,6 @@ docker-compose -f docker-stack.yml exec db psql -h db -d postgres -U postgres -c
 docker-compose -f docker-stack.yml exec db psql -h db -d postgres -U postgres -c "CREATE DATABASE tests ENCODING 'UTF8'"
 docker-compose -f docker-stack.yml restart backend
 docker-compose -f docker-stack.yml exec backend-tests wait-for-it -t 10 backend:80
-docker-compose -f docker-stack.yml exec -T backend-tests pytest ./tests -vvv -m "ui or integration" --junitxml="${TEST_RESULTS}" --driver Chrome --base-url http://frontend 
+docker-compose -f docker-stack.yml exec -T backend-tests pytest ./tests -vvv -m "integration and ui" --junitxml="${TEST_RESULTS}" --base-url ${BASE_URL}
 # Comment this line out to leave the stack running. Useful for test development.
 docker-compose -f docker-stack.yml down -v --remove-orphans
