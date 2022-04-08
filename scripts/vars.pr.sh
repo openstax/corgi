0b6ab7c1b0192bf982b854ff7d4f954e49acb051
m1yag1 <mike.arbelaez@rice.edu>
2022-04-08 00:29:09 -0500

    Added docker-compose.stack.pr.yml for deploying pull request environments

---
+++ scripts/vars.pr.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+[ "${PR_NUMBER}" = '' ] && echo "ERROR: Remember to set paramater for PR_NUMBER. e.g. PR_NUMBER=458" && exit 1
+
+export DOMAIN="corgi-$PR_NUMBER.ce.openstax.org"
+export STACK_NAME="corgi_$PR_NUMBER"
+export TRAEFIK_TAG="traefik-$PR_NUMBER"
+
+NEWLINE=$'\n'
+echo "${NEWLINE}The following environment variables were set:${NEWLINE}"
+echo "DOMAIN=$DOMAIN"
+echo "STACK_NAME=$STACK_NAME"
+echo "TRAEFIK_TAG=$TRAEFIK_TAG"
+echo "${NEWLINE}"
