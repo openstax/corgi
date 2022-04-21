828802dd06baa2919d0a1ffb2197160748c23fe1
m1yag1 <mike.arbelaez@rice.edu>
2022-04-21 10:40:30 -0500

    Update scripts/tests.ci.sh to use run ui and integration tests separately
    * .gitignore the new junit-xml files created
    
    * Updated circleci config to run updated tests script and reports

--- scripts/tests.ci.sh
+++ scripts/tests.ci.sh
@@ -3,12 +3,15 @@
 # Exit in case of error
 set -e
 
-TEST_RESULTS=${TEST_RESULTS-./junit.xml}
+TEST_RESULTS_DIR=${TEST_RESULTS_DIR-./reports}
+TEST_RESULTS_INTEGRATION=${TEST_RESULTS_INTEGRATION-./junit-integration.xml}
+TEST_RESULTS_UNIT=${TEST_RESULTS_UNIT-./junit-unit.xml}
+TEST_RESULTS_UI=${TEST_RESULTS_UI-./junit-ui.xml}
 
-echo "Test results will be saved in: ${TEST_RESULTS}"
+echo "Test results will be saved in: ${TEST_RESULTS_DIR}"
 
-DOMAIN=${DOMAIN-backend}
-BASE_URL=${FQDN-http://backend}
+BACKEND_URL="http://backend"
+FRONTEND_URL="http://frontend"
 DEPLOYED_AT=$(date '+%Y%m%d.%H%M%S')
 REVISION=$(git --git-dir=./.git rev-parse --short HEAD)
 
@@ -29,6 +32,7 @@ docker-compose -f docker-stack.yml exec db psql -h db -d postgres -U postgres -c
 docker-compose -f docker-stack.yml exec db psql -h db -d postgres -U postgres -c "CREATE DATABASE tests ENCODING 'UTF8'"
 docker-compose -f docker-stack.yml restart backend
 docker-compose -f docker-stack.yml exec backend-tests wait-for-it -t 10 backend:80
-docker-compose -f docker-stack.yml exec -T backend-tests pytest -m "ui or integration" -vvv --junitxml="${TEST_RESULTS}" --base-url="${BASE_URL}"
+docker-compose -f docker-stack.yml exec -T backend-tests pytest -vvv --junitxml="${TEST_RESULTS_INTEGRATION}" --base-url="${BACKEND_URL}" ./tests/integration
+docker-compose -f docker-stack.yml exec -T backend-tests pytest -vvv --junitxml="${TEST_RESULTS_UI}" --base-url="${FRONTEND_URL}" ./tests/ui
 # Comment this line out to leave the stack running. Useful for test development.
 docker-compose -f docker-stack.yml down -v --remove-orphans
