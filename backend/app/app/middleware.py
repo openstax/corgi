0041d3933fb5e1b701ba0ef1d6d890be6008d7db
m1yag1 <mike.arbelaez@rice.edu>
2022-05-03 09:37:04 -0500

    Wrap DBSessionMiddleware request in try except block
    * We wrap the DBSessionMiddleware in try/except/finally block because we
      could throw any number of exceptions. Currently, we're seeing a
      Connection error in SQLAlchemy due to unclosed connections.
    * To help with the unclosed connection we've added a finally clause to
      ensure we always close the connection.

--- backend/app/app/middleware.py
+++ backend/app/app/middleware.py
@@ -1,5 +1,6 @@
 import logging
 
+from fastapi import Response
 from starlette.middleware.base import BaseHTTPMiddleware
 
 from app.db.session import Session
@@ -7,9 +8,17 @@ from app.db.session import Session
 
 class DBSessionMiddleware(BaseHTTPMiddleware):
     async def dispatch(self, request, call_next):
-        logging.info(f"Creating Database session for {request.url}")
-        request.state.db = Session()
-        response = await call_next(request)
-        logging.info(f"Closing Database session for {request.url}")
-        request.state.db.close()
+        logging.debug(f"Creating Database session for {request.url}")
+        response = Response("Internal server error", status_code=500)
+        # https://fastapi.tiangolo.com/tutorial/sql-databases/#create-a-middleware
+        try: 
+            request.state.db = Session()
+            response = await call_next(request)
+        # We log the exception because it could be one of many.
+        except Exception(e):
+            logging.exception(e)
+        # Always close the db session, even after an exception
+        finally:
+            logging.debug(f"Closing Database session for {request.url}")
+            request.state.db.close()
         return response
