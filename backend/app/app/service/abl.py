bca0b492d7cf1ee59ba3ae74fd6e83d05188e73d
Tyler Nullmeier <tylerzeromaster@gmail.com>
2022-06-06 15:32:50 -0500

    Fix some typos and pass headers as kw argument

--- backend/app/app/service/abl.py
+++ backend/app/app/service/abl.py
@@ -17,36 +17,36 @@ async def get_abl_info(repo_name, slug, version="main"):
 
 def get_abl_line_number(repo_name):
     abl_file = requests.get("https://raw.githubusercontent.com/openstax/content-manager-approved-books/main/approved-book-list.json").text.split("\n")
-    for line_number, line  in enumerate(abl_file):
+    for line_number, line in enumerate(abl_file):
         if repo_name in line:
             return line_number + 1
     return 1
 
 def get_book_metadata(repo_name, slug, version="main"):
     # commit_sha
-    refs = requests.get(f"https://api.github.com/repos/openstax/{repo_name}/git/refs/heads/{version}", headers)
+    refs = requests.get(f"https://api.github.com/repos/openstax/{repo_name}/git/refs/heads/{version}", headers=headers)
     refs_obj = json.loads(refs.content)
     commit_sha = refs_obj["object"]["sha"]
 
     # committed_at
-    commit = requests.get(f"https://api.github.com/repos/openstax/{repo_name}/commits/{commit_sha}", headers)
+    commit = requests.get(f"https://api.github.com/repos/openstax/{repo_name}/commits/{commit_sha}", headers=headers)
     commit_obj = json.loads(commit.content)
     commit_timestamp = commit_obj["commit"]["committer"]["date"]
-    fixed_timetamp = f"{commit_timestamp[:-1]}+00:00"
+    fixed_timestamp = f"{commit_timestamp[:-1]}+00:00"
     
     # style
-    meta_inf = get_public_git_file("openstax", repo_name, "META-INF/books.xml", version)
+    meta_inf = get_git_file("openstax", repo_name, "META-INF/books.xml", version)
     meta = etree.fromstring(meta_inf)
     style = meta.xpath(f"//*[local-name()='book'][@slug='{slug}']")[0].attrib["style"]
 
     # uuid    
-    collection_xml = get_public_git_file("openstax", repo_name, f"/collections/{slug}.collection.xml", version)
+    collection_xml = get_git_file("openstax", repo_name, f"/collections/{slug}.collection.xml", version)
     collection = etree.fromstring(collection_xml)
     uuid = collection.xpath("//*[local-name()='uuid']")[0].text
 
     metadata = {
         "commit_sha": commit_sha,
-        "committed_at": fixed_timetamp,
+        "committed_at": fixed_timestamp,
         "uuid": uuid,
         "style": style
     }
@@ -54,7 +54,7 @@ def get_book_metadata(repo_name, slug, version="main"):
     return metadata
 
 def get_git_file(owner, repo_name, path, version):
-    git_metadata = requests.get(f"https://api.github.com/repos/{owner}/{repo_name}/contents/{path}?ref={version}", headers)
+    git_metadata = requests.get(f"https://api.github.com/repos/{owner}/{repo_name}/contents/{path}?ref={version}", headers=headers)
     base64_content = json.loads(git_metadata.content)["content"]
     content = b64decode(base64_content)
     return content
