e35d59116099f224c9190baa90a1dea3203807ea
m1yag1 <mike.arbelaez@rice.edu>
2022-06-07 11:25:14 -0500

    Added nondestructive and sensitive config/markers to fix errors

--- backend/app/tests/conftest.py
+++ backend/app/tests/conftest.py
@@ -1,3 +1,7 @@
+import re
+
+import pytest
+
 # Import fixtures
 pytest_plugins = "tests.ui.fixtures.ui"
 
@@ -12,10 +16,46 @@ def get_custom_markers():
         "smoke: mark tests used for smoke testing",
         "testrail: mark tests used for testrail runs",
         "ui: mark tests used for ui tests",
-        "unit: mark tests as a unit test"
+        "unit: mark tests as a unit test",
+        "nondestructive: tests using this decorator will run in sensitive environments",
+        "sensitive: the url for environments where destructive tests should not be executed"
     )
+
+
+def pytest_addoption(parser):
+    group = parser.getgroup("safety", "safety")
+    group.addoption(
+        "--destructive",
+        action="store_true",
+        dest="run_destructive",
+        default=False,
+        help="include destructive tests (tests not explicitly marked as \'nondestructive\'). (disabled by default).",
+    )
+    group._addoption('--sensitiveurl',
+                    action='store',
+                    dest='sensitive_url',
+                    default='corgi\.ce\.openstax\.org',
+                    metavar='str',
+                    help='regular expression for identifying sensitive urls.')
 
 
 def pytest_configure(config):
     for marker in get_custom_markers():
         config.addinivalue_line("markers", marker)
+
+    if not config.option.run_destructive:
+        if config.option.markexpr:
+            config.option.markexpr = f"nondestructive and {config.option.markexpr}"
+        else:
+            config.option.markexpr = "nondestructive"
+
+
+def pytest_runtest_setup(item):
+    sensitive = re.search(item.config.option.sensitive_url, item.config.option.base_url)
+    destructive = 'nondestructive' not in item.keywords
+
+    if (sensitive and destructive):
+        # Skip the test with an appropriate message
+        pytest.skip("This test is destructive and the target URL is" \
+                    "considered a sensitive environment. If this test" \
+                    "is not destructive add the 'nondestructive' marker.")
