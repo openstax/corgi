b414b34c5f4a87066e1f64f7f0d530551ae38e55
Otto Mehes <om9@rice.edu>
2022-08-03 23:45:05 -0500

    CI fail fix - experiment - click_create_job REMOVED

--- backend/app/tests/ui/fixtures/ui.py
+++ backend/app/tests/ui/fixtures/ui.py
@@ -7,7 +7,7 @@ def chrome_page():
     """Return playwright chromium browser page"""
     playwright_sync = sync_playwright().start()
     chrome_browser = playwright_sync.chromium.launch(
-        headless=False, slow_mo=1000, timeout=99999
+        headless=True, slow_mo=1000, timeout=99999
     )
     page = chrome_browser.new_page()
     yield page
