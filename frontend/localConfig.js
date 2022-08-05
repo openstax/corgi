d3f840a10eb7cfbebd6ac08d9e820fe1ae53c238
therealmarv <1050582+therealmarv@users.noreply.github.com>
2022-08-05 21:33:21 +0300

    use staging as default when nothing is configured

--- frontend/localConfig.js
+++ frontend/localConfig.js
@@ -1,4 +1,4 @@
 export default {
-  apiBaseURL: process.env.API_URL || 'https://corgi.openstax.org',
-  browserBaseURL: process.env.API_URL_BROWSER || 'https://corgi.openstax.org'
+  apiBaseURL: process.env.API_URL || 'https://corgi-staging.ce.openstax.org',
+  browserBaseURL: process.env.API_URL_BROWSER || 'https://corgi-staging.ce.openstax.org'
 }
