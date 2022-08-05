8481aeb4d20cca4025662cdf1f1426e0704d5485
therealmarv <1050582+therealmarv@users.noreply.github.com>
2022-08-05 21:43:45 +0300

    add more comments

--- frontend/localConfig.js
+++ frontend/localConfig.js
@@ -1,7 +1,7 @@
 export default {
   // Docs: https://axios.nuxtjs.org/options/#browserbaseurl
   // apiBaseURL is used for the server part of nuxt (running in node on server).
-  // E.g. internal networking can be used on apiBaseURL.
+  // E.g. internal networking can be used on apiBaseURL for axios requests on server.
   // browserBaseURL will be used on axios requests which are ran on client=browser side.
   // browserBaseURL needs to be public reachable on the internet!
   apiBaseURL: process.env.API_URL || 'https://corgi-staging.ce.openstax.org',
