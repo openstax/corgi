a26a5c9965916bdbc19439a468afa1c04a55ce8d
Tyler Nullmeier <tylerzeromaster@gmail.com>
2022-09-20 16:41:40 -0500

    Use authlib instead of a DIY solution, move... auth config to global config, add role checking, and cleanup some files that were rendered useless

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -1,4 +1,7 @@
 import os
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # DATABASE SETTINGS
 POSTGRES_SERVER = os.getenv("POSTGRES_SERVER")
@@ -25,3 +28,23 @@ DEPLOYED_AT = os.getenv("DEPLOYED_AT")
 
 # GITHUB SETTINGS
 GITHUB_API_TOKEN = os.getenv("GITHUB_API_TOKEN")
+
+# GITHUB OAUTH
+CLIENT_ID = os.getenv("CLIENT_ID")
+CLIENT_SECRET = os.getenv("CLIENT_SECRET")
+ADMIN_TEAMS = ("ce-tech", "ce-admins", "content-managers")
+GITHUB_LOGIN_URL = (
+    "https://github.com/login/oauth/authorize?"
+    f"client_id={CLIENT_ID}&scope=read:user%20read:org"
+)
+
+# JWT
+# tr -dc 'a-zA-Z0-9[:punct:]' < /dev/urandom | dd bs=1000 count=1 2>/dev/null
+SESSION_SECRET = (
+    "cMzeqhQ2HsM2LFPRJBy43kNdqv9RNEMMxtmk3RmLNN5M1bfhKR4ofgXe8DpzIjUl"
+    "DGjPJU0L6webMfMIAz9pWlEPc2CkroTaOuOesASoihqb1J586YxCDjdI7jJ9ZVpB"
+    "fvyPPHyVKrnIYFnQDoof2Z9GTR8TIKqt6J2qtQj3QlUWvMj9PyMAh4CJq7lEjCAR"
+    "jJqkWY6nG4HAQl5f6WeOYUmNiDEqRvSuwSQhcNjVgoz4vxKQorqni2C5JBvXY0Te"
+)
+ALGORITHM = "HS256"
+ACCESS_TOKEN_EXPIRE_MINUTES = 30
