a023825f832c90ea173dc954fce6e1f8a0b53dcd
Tyler Nullmeier <tylerzeromaster@gmail.com>
2022-11-16 17:06:04 -0600

    Move user repository operations to user service
    Additionally, move filtering into sync_user_data and filter by
    permission when repositories are inserted on login rather than on
    select/fetch.
    
    I foresee this being temporary, but it gets us closer to the
    functionality we want now and helps move filtering out of services.

--- backend/app/app/github/utils.py
+++ backend/app/app/github/utils.py
@@ -3,7 +3,7 @@ from app.db.schema import Repository
 from app.github.api import get_user_repositories
 from app.github.client import AuthenticatedClient
 from app.service.repository import repository_service
-from app.service.user import user_service
+from app.service.user import user_service, RepositoryPermission
 from sqlalchemy.orm import Session
 
 
@@ -13,11 +13,15 @@ async def sync_user_data(client: AuthenticatedClient,
 
     user_repos = await get_user_repositories(
         client, "org:openstax osbooks in:name")
-    user_repos = [ur for ur in user_repos if ur.name.startswith("osbooks-")]
+    user_repos = [r for r in user_repos if r.name.startswith("osbooks-")]
+    if not user.is_admin():
+        user_repos = [
+            r for r in user_repos if r.viewer_permission in (
+                RepositoryPermission.ADMIN.name,
+                RepositoryPermission.WRITE.name)]
     user_service.upsert_user(db, user)
     repository_service.upsert_repositories(db, [
         Repository(id=repo.database_id, name=repo.name, owner="openstax")
         for repo in user_repos
     ])
-    repository_service.upsert_user_repositories(db, user.id, user_repos)
-
+    user_service.upsert_user_repositories(db, user, user_repos)
--- backend/app/app/service/repository.py
+++ backend/app/app/service/repository.py
@@ -1,57 +1,18 @@
-from enum import Enum
 from typing import List
 
-from app.core.auth import Role
 from app.data_models.models import Repository as RepositoryModel
-from app.data_models.models import UserSession
 from app.db.schema import Repository as RepositorySchema
-from app.db.schema import UserRepository
-from app.github import GitHubRepo
 from app.service.base import ServiceBase
 from sqlalchemy.orm import Session
 
 
-class RepositoryPermission(int, Enum):
-    ADMIN = 1
-    MAINTAIN = 2
-    READ = 3
-    TRIAGE = 4
-    WRITE = 5
-
-
 class RepositoryService(ServiceBase):
-    def get_user_repositories(
-        self,
-        db: Session,
-        user: UserSession
-    ) -> List[RepositoryModel]:
-        query = db.query(UserRepository)
-        #Ideally, this role check would be in a more auth-centric location
-        if not user.is_admin():
-            query = query.filter(
-                UserRepository.user_id == user.id,
-                UserRepository.permission_id.in_([
-                    RepositoryPermission.ADMIN,
-                    RepositoryPermission.WRITE
-                ])
-            )
-        else:
-            query = query = query.filter(UserRepository.user_id == user.id)
-        return [ur.repository for ur in query.all()]
-
-    def upsert_repositories(self, db: Session, 
-                            repositories: List[RepositorySchema]):
+    def upsert_repositories(
+            self,
+            db: Session,
+            repositories: List[RepositorySchema]):
         for repo in repositories:
             db.merge(repo)
-        db.commit()
-    
-    def upsert_user_repositories(self, db: Session, user_id: int, 
-                                 repo_list: List[GitHubRepo]):
-        for repo in repo_list:
-            permission_id = RepositoryPermission[repo.viewer_permission]
-            db.merge(UserRepository(user_id=user_id, 
-                                    permission_id=permission_id,
-                                    repository_id=repo.database_id))
         db.commit()
 
 
