716f27579adb3ab868e3288473c20062396a9806
Tyler Nullmeier <tylerzeromaster@gmail.com>
2022-12-16 13:07:33 -0600

    Add tests for cookie encryption

--- backend/app/tests/unit/api/endpoints/test_login.py
+++ backend/app/tests/unit/api/endpoints/test_login.py
@@ -1,38 +1,67 @@
-import pytest
+import json
+from base64 import b64decode
 
+import pytest
 from app.core.config import ACCESS_TOKEN_EXPIRE_MINUTES
 from app.core.errors import CustomBaseError
 from app.github.api import AccessDeniedError
 
 
+def check_cookie_value(cookie):
+    from app.core.auth import Crypto
+    assert cookie is not None
+    assert "session=" in cookie
+    assert str(ACCESS_TOKEN_EXPIRE_MINUTES * 60) in cookie
+    cookie_values = cookie.split(";")
+    session_cookie = None
+    for value in cookie_values:
+        if "session=" in value:
+            # Get the value without 'session=' and parse it
+            # The session cookie is made up of 3 values delimited by "."
+            # The last two values are related to signing
+            # The first value is what we stored
+            session_cookie = json.loads(b64decode(value[8:].split(".")[0]))
+    assert session_cookie is not None
+    assert "user" in session_cookie
+    user_cookie = session_cookie["user"]
+    assert "exp" in user_cookie
+    # The nested json should be encrypted. Trying to decode it should not work.
+    with pytest.raises(json.JSONDecodeError):
+        json.loads(user_cookie["session"])
+    # if we decrypt it first, however, it should work
+    user_session = json.loads(Crypto.decrypt(user_cookie["session"]))
+    assert "id" in user_session
+    assert "token" in user_session
+    assert "role" in user_session
+    assert "avatar_url" in user_session
+    assert "name" in user_session
+
+
+
 @pytest.mark.unit
 @pytest.mark.nondestructive
 def test_login_success(testclient, mock_login_success):
-    response = testclient.get(f"/api/auth/login", allow_redirects=False)
+    response = testclient.get("/api/auth/login", allow_redirects=False)
     assert response.status_code == 307
     redirect_location = response.headers.get("location")
     assert redirect_location is not None
     response = testclient.get(redirect_location, allow_redirects=False)
     assert response.status_code == 307
     cookie = response.headers.get("set-cookie")
-    assert cookie is not None
-    assert "session=" in cookie
-    assert str(ACCESS_TOKEN_EXPIRE_MINUTES * 60) in cookie
-
+    check_cookie_value(cookie)
+    
 
 @pytest.mark.unit
 @pytest.mark.nondestructive
 def test_login_success_token(testclient, mock_login_success):
     response = testclient.get(
-        f"/api/auth/token-login",
+        "/api/auth/token-login",
         allow_redirects=False,
         headers={
             "authorization": "Bearer fake-token"})
     assert response.status_code == 200
     cookie = response.headers.get("set-cookie")
-    assert cookie is not None
-    assert "session=" in cookie
-    assert str(ACCESS_TOKEN_EXPIRE_MINUTES * 60) in cookie
+    check_cookie_value(cookie)
 
 
 @pytest.mark.unit
