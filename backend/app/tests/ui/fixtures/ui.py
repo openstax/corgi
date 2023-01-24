053774a553812268c1d257749661ff656232b6ea
Ottó <44207656+omehes@users.noreply.github.com>
2023-01-24 13:59:13 -0600

    QA tests for the new corgi UI (#523)
    * try to use current ubuntu image in CircleCI
    * use no terminal in CircleCI tests for poetry
    see
    https://github.com/python-poetry/poetry/issues/7184
    for details of the issue.
    
    * added testrail case numbers and parametrize
    * removed all mark.smoke marker - not needed
    * added more testrail case numbers
    Co-authored-by: therealmarv <1050582+therealmarv@users.noreply.github.com>

--- backend/app/tests/ui/fixtures/ui.py
+++ backend/app/tests/ui/fixtures/ui.py
@@ -7,9 +7,27 @@ def chrome_page():
     """Return playwright chromium browser page"""
     playwright_sync = sync_playwright().start()
     chrome_browser = playwright_sync.chromium.launch(
-        headless=True, slow_mo=750, timeout=99999
+        headless=True, slow_mo=800, timeout=120000
     )
-    page = chrome_browser.new_page()
+    context = chrome_browser.new_context(storage_state="state.json")
+
+    page = context.new_page()
+    yield page
+
+    chrome_browser.close()
+    playwright_sync.stop()
+
+
+@pytest.fixture
+def chrome_page_slow():
+    """Return playwright chromium browser page - slow flow"""
+    playwright_sync = sync_playwright().start()
+    chrome_browser = playwright_sync.chromium.launch(
+        headless=True, slow_mo=3700, timeout=120000
+    )
+    context = chrome_browser.new_context(storage_state="state.json")
+
+    page = context.new_page()
     yield page
 
     chrome_browser.close()
@@ -19,4 +37,5 @@ def chrome_page():
 @pytest.fixture
 def corgi_base_url(base_url):
     """Return local corgi url"""
+    base_url = "https://corgi-514.ce.openstax.org/"
     return base_url
---
+++ backend/app/tests/ui/test_multiple_error_messages.py
@@ -0,0 +1,181 @@
+import pytest
+from pytest_testrail.plugin import pytestrail
+
+from pages.home import HomeCorgi
+
+
+@pytestrail.case("C624696")
+@pytest.mark.ui
+@pytest.mark.nondestructive
+@pytest.mark.parametrize(
+    "repo, book, version",
+    [("osbooks-astronomy", "astro", "main")],
+)
+def test_incorrect_book_and_version_error_messages(chrome_page, corgi_base_url, repo, book, version):
+    # GIVEN: Playwright, chromium and the corgi_base_url
+
+    # WHEN: The Home page is fully loaded
+    chrome_page.goto(corgi_base_url)
+    home = HomeCorgi(chrome_page)
+
+    # WHEN: Input fields are filled and a job check box is selected
+    home.fill_repo_field(repo)
+    home.fill_book_field(book)
+    home.fill_version_field(version)
+
+    home.click_webview_job_option()
+
+    # WHEN: Create new job button is clicked
+    home.click_create_new_job_button()
+
+    # THEN: A new job is NOT queued and multiple error dialogs appear
+    assert "Error: Book not in repository 'astro'" in home.error_message_dialog_locator.text_content()
+
+    home.fill_version_field("p888p")
+
+    home.click_create_new_job_button()
+
+    assert "Error: Book not in repository 'astro'" and "Error: Could not find commit 'p888p'" in home.error_message_dialog_locator.text_content()
+
+    home.click_error_banner_okay_button()
+
+
+@pytest.mark.ui
+@pytest.mark.nondestructive
+@pytest.mark.parametrize(
+    "repo, book, version",
+    [("osbooks-astronomy", "astro", "main")],
+)
+def test_incorrect_repo_and_book_error_messages(chrome_page, corgi_base_url, repo, book, version):
+    # GIVEN: Playwright, chromium and the corgi_base_url
+
+    # WHEN: The Home page is fully loaded
+    chrome_page.goto(corgi_base_url)
+    home = HomeCorgi(chrome_page)
+
+    # WHEN: Input fields are filled and a job check box is selected
+    home.fill_repo_field(repo)
+    home.fill_book_field(book)
+    home.fill_version_field(version)
+
+    home.click_pdf_job_option()
+
+    # WHEN: Create new job button is clicked
+    home.click_create_new_job_button()
+
+    # THEN: A new job is NOT queued and multiple error dialogs appear
+    assert "Error: Book not in repository 'astro'" in home.error_message_dialog_locator.text_content()
+
+    home.fill_repo_field("osbooks-astron")
+
+    assert "Error: Could not resolve to a Repository with the name 'openstax/osbooks-astron'" and "Error: Book not in repository 'astro'" in home.error_message_dialog_locator.text_content()
+
+    home.click_error_banner_okay_button()
+
+
+@pytestrail.case("C624695")
+@pytest.mark.ui
+@pytest.mark.nondestructive
+@pytest.mark.parametrize(
+    "repo, book, version",
+    [("osbooks-astronomy", "astronomy-2e", "p888p")],
+)
+def test_incorrect_repo_and_version_error_messages(chrome_page, corgi_base_url, repo, book, version):
+    # GIVEN: Playwright, chromium and the corgi_base_url
+
+    # WHEN: The Home page is fully loaded
+    chrome_page.goto(corgi_base_url)
+    home = HomeCorgi(chrome_page)
+
+    # WHEN: Input fields are filled and a job check box is selected
+    home.fill_repo_field(repo)
+    home.fill_book_field(book)
+    home.fill_version_field(version)
+
+    home.click_docx_job_option()
+
+    # WHEN: Create new job button is clicked
+    home.click_create_new_job_button()
+
+    # THEN: A new job is NOT queued and multiple error dialogs appear
+    assert "Error: Could not find commit 'p888p'" in home.error_message_dialog_locator.text_content()
+
+    home.fill_repo_field("osbooks-astron")
+
+    assert "Error: Could not resolve to a Repository with the name 'openstax/osbooks-astron'" and "Error: Could not find commit 'p888p'" in home.error_message_dialog_locator.text_content()
+
+    home.click_error_banner_okay_button()
+
+
+@pytest.mark.ui
+@pytest.mark.nondestructive
+@pytest.mark.parametrize(
+    "repo, book, version",
+    [("osbooks-astronomy", "astro", "main")],
+)
+def test_incorrect_repo_book_and_version_error_messages(chrome_page, corgi_base_url, repo, book, version):
+    # GIVEN: Playwright, chromium and the corgi_base_url
+
+    # WHEN: The Home page is fully loaded
+    chrome_page.goto(corgi_base_url)
+    home = HomeCorgi(chrome_page)
+
+    # WHEN: Input fields are filled and a job check box is selected
+    home.fill_repo_field(repo)
+    home.fill_book_field(book)
+    home.fill_version_field(version)
+
+    home.click_docx_job_option()
+    home.click_pdf_job_option()
+    home.click_webview_job_option()
+
+    # WHEN: Create new job button is clicked
+    home.click_create_new_job_button()
+
+    # THEN: A new job is NOT queued and multiple error dialogs appear
+    assert "Error: Book not in repository 'astro'" in home.error_message_dialog_locator.text_content()
+
+    home.fill_version_field("p888p")
+    home.fill_repo_field("osbooks-astron")
+
+    assert "Error: Could not resolve to a Repository with the name 'openstax/osbooks-astron'" and "Error: Book not in repository 'astro'" in home.error_message_dialog_locator.text_content()
+
+    home.click_error_banner_okay_button()
+
+
+@pytest.mark.ui
+@pytest.mark.nondestructive
+@pytest.mark.parametrize(
+    "repo, book, version",
+    [("osbooks-astronomy", "astronomy-2e", "p888p")],
+)
+def test_incorrect_repo_and_version_error_messages_no_okay(chrome_page, corgi_base_url, repo, book, version):
+    # GIVEN: Playwright, chromium and the corgi_base_url
+
+    # WHEN: The Home page is fully loaded
+    chrome_page.goto(corgi_base_url)
+    home = HomeCorgi(chrome_page)
+
+    # WHEN: Input fields are filled and a job check box is selected
+    home.fill_repo_field(repo)
+    home.fill_book_field(book)
+    home.fill_version_field(version)
+
+    home.click_docx_job_option()
+
+    # WHEN: Create new job button is clicked
+    home.click_create_new_job_button()
+
+    # THEN: A new job is NOT queued and multiple error dialogs appear
+    assert "Error: Could not find commit 'p888p'" in home.error_message_dialog_locator.text_content()
+
+    home.fill_repo_field("osbooks-astron")
+    home.click_version_field()
+
+    assert "Error: Could not resolve to a Repository with the name 'openstax/osbooks-astron'" and "Error: Could not find commit 'p888p'" in home.error_message_dialog_locator.text_content()
+
+    home.click_create_new_job_button()
+
+    assert "Error: Could not resolve to a Repository with the name 'openstax/osbooks-astron'" and "Error: Could not find commit 'p888p'" in home.error_message_dialog_locator.text_content()
+
+    home.click_error_banner_okay_button()
