1d9851e94a548b018adcd414603397c8c9bf6e63
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-03-07 00:24:33 +0000

    Automatically create session secret: no .env required

--- backend/app/bin/live-reload.sh
+++ backend/app/bin/live-reload.sh
@@ -4,5 +4,7 @@ cd /app/app || exit
 
 MODULE_NAME=${MODULE_NAME:-main}
 CALLABLE_NAME=${CALLABLE_NAME:-server}
+SESSION_SECRET="$(dd if=/dev/urandom bs=1024 count=1 2>/dev/null | base64)"
+export SESSION_SECRET
 
 uvicorn "${MODULE_NAME}:${CALLABLE_NAME}" --host 0.0.0.0 --port 80 --debug
