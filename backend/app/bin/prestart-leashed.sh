7870da8665d4b4fcdc8a28c737e853c063d98ae4
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-03-07 15:33:32 -0600

    DRY: prestart-leashed

--- backend/app/bin/prestart-leashed.sh
+++ backend/app/bin/prestart-leashed.sh
@@ -1,12 +1,6 @@
 #! /usr/bin/env bash
 
-# Let the DB start
-python ./bin/db_wait.py
-
-# Run migrations
-alembic upgrade head
-
 export MODULE_NAME=leashed
 
-# shellcheck source=SCRIPTDIR/live-reload.sh
-source ./bin/live-reload.sh
+# shellcheck source=SCRIPTDIR/prestart-dev.sh
+source ./bin/prestart-dev.sh
