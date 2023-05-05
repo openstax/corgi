b1b81ef62008ad064a6d16b2e86350e23487ec50
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-05-05 13:31:03 -0500

    Add bootcamp to frontend

---
+++ frontend/bootcamp/app.js
@@ -0,0 +1,81 @@
+const { exec } = require("child_process");
+const fs = require("fs");
+const path = require("path");
+const express = require("express");
+
+const app = express();
+const repoPath = "/corgi";
+const corgiRepo = "https://github.com/openstax/corgi";
+const frontendRepoDir = path.join(repoPath, "frontend");
+const frontendDir = "/app";
+
+function sh(cmd, ignoreExitCode = false) {
+  return new Promise((resolve, reject) => {
+    exec(cmd, (error, stdout, stderr) => {
+      if (!ignoreExitCode && error != null && error.code !== 0) {
+        reject(new Error(stderr));
+      } else {
+        resolve({ error, stdout, stderr });
+      }
+    });
+  });
+}
+
+function scopedGit(cmd, scope = repoPath) {
+  return sh(`git -C ${scope} ${cmd}`);
+}
+
+function gitInit() {
+  if (!fs.existsSync(repoPath)) {
+    fs.mkdirSync(repoPath, { recursive: true });
+    return sh(`git -C ${path.dirname(repoPath)} clone ${corgiRepo}`);
+  }
+  return Promise.resolve();
+}
+
+function copyDirectory(src, dest) {
+  if (!fs.existsSync(dest)) {
+    fs.mkdirSync(dest, { recursive: true });
+  }
+
+  const entries = fs.readdirSync(src, { withFileTypes: true });
+
+  for (const entry of entries) {
+    const srcPath = path.join(src, entry.name);
+    const destPath = path.join(dest, entry.name);
+
+    if (entry.isDirectory()) {
+      copyDirectory(srcPath, destPath);
+    } else {
+      fs.copyFileSync(srcPath, destPath);
+    }
+  }
+}
+
+// Enable JSON request body decoding
+app.use(express.json());
+
+app.post("/checkout", async (req, res) => {
+  const { commit } = req.body;
+  if (commit == null) {
+    res.status(500).send("Need commit to checkout");
+  } else {
+    try {
+      await scopedGit("fetch");
+      await scopedGit(`checkout ${commit}`);
+      copyDirectory(frontendRepoDir, frontendDir);
+      res.sendStatus(200);
+    } catch (err) {
+      console.error(err);
+      res.sendStatus(500);
+    }
+  }
+});
+
+const PORT = process.env.PORT || 3000;
+gitInit().catch((err) => {
+  throw err;
+});
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
