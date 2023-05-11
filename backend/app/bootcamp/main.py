142b17581af33d9036e7570f8791f673c7f3cbad
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-05-11 13:12:34 -0500

    Add persistence to bundle

--- backend/app/bootcamp/main.py
+++ backend/app/bootcamp/main.py
@@ -4,8 +4,10 @@ from shutil import copytree, ignore_patterns
 from subprocess import run, PIPE
 from shlex import split
 from typing import Optional
+from enum import Enum
 from time import time
 import re
+import json
 
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
@@ -16,6 +18,11 @@ import httpx
 server = FastAPI(title="CORGI Bootcamp")
 
 
+class StatusType(str, Enum):
+    running = "running"
+    building = "building"
+
+
 class Head(BaseModel):
     corgi_ref: Optional[str]
     enki_ref: Optional[str]
@@ -23,8 +30,69 @@ class Head(BaseModel):
     enki_modified: Optional[float]
 
 
+class Status(BaseModel):
+    enki_status: StatusType
+
+
 class Bundle(BaseModel):
     head: Head
+    status: Status
+
+    def update(
+        self, head: Optional[Head] = None, status: Optional[Status] = None
+    ):
+        return Bundle(
+            head=head if head is not None else self.head,
+            status=status if status is not None else self.status,
+        )
+
+    @staticmethod
+    def default():
+        return Bundle(
+            head=Head(
+                corgi_ref="main",
+                enki_ref="main",
+                corgi_modified=time(),
+                enki_modified=time(),
+            ),
+            status=Status(enki_status=StatusType.running),
+        )
+
+
+class BundleManager:
+    __slots__ = ("_bundle", "_bundle_path")
+
+    def __init__(self, bundle_path: str):
+        self._bundle: Optional[Bundle] = None
+        self._bundle_path = bundle_path
+
+    def _save(self, bundle: Bundle):
+        with open(self._bundle_path, "w") as fout:
+            fout.write(bundle.json())
+        self._bundle = bundle
+
+    def save(
+        self, head: Optional[Head] = None, status: Optional[Status] = None
+    ):
+        self._save(self.bundle.update(head, status))
+
+    @property
+    def bundle(self) -> Bundle:
+        if self._bundle is not None:
+            return self._bundle
+
+        bundle = Bundle.default()
+        try:
+            default_keys = Bundle.__fields__.keys()
+            with open(self._bundle_path) as fin:
+                saved = json.load(fin)
+            bundle = bundle.update(
+                **{k: v for k, v in saved.items() if k in default_keys}
+            )
+        except Exception:
+            self._save(bundle)
+
+        return bundle
 
 
 # NOTE: These global variables will not work if multiple instances are running
@@ -34,14 +102,7 @@ REPO_PATH = "/corgi"
 BACKEND_REPO_DIR = os.path.join(REPO_PATH, "backend", "app")
 BACKEND_DIR = "/app"
 REF_REGEX = re.compile(r"^[a-zA-Z0-9_./-]+$")
-saved_bundle = Bundle(
-    head=Head(
-        corgi_ref="main",
-        enki_ref="main",
-        corgi_modified=time(),
-        enki_modified=time(),
-    )
-)
+BUNDLE_MGR = BundleManager("/persist/bundle.json")
 
 
 def sh(cmd: str, ignore_exitcode=False):
@@ -63,16 +124,6 @@ def is_pullable(ref):
     )
 
 
-def save(*, head: Optional[Head] = None):
-    global saved_bundle
-    prev = load()
-    saved_bundle = Bundle(head=head if head is not None else prev.head)
-
-
-def load():
-    return saved_bundle
-
-
 def _corgi_checkout(ref):
     scoped_git("fetch")
     scoped_git(f"checkout {ref}")
@@ -90,7 +141,7 @@ def _corgi_checkout(ref):
 
 @server.post("/checkout")
 def corgi_checkout(checkout_request: Head):
-    saved = load().head
+    saved = BUNDLE_MGR.bundle.head
     corgi_ref = saved.corgi_ref
     enki_ref = saved.enki_ref
     corgi_modified = saved.corgi_modified
@@ -110,7 +161,7 @@ def corgi_checkout(checkout_request: Head):
             raise Exception("Unsupported ref")
         enki_ref = checkout_request.enki_ref
         enki_modified = time()
-    save(
+    BUNDLE_MGR.save(
         head=Head(
             corgi_ref=corgi_ref,
             corgi_modified=corgi_modified,
@@ -122,12 +173,12 @@ def corgi_checkout(checkout_request: Head):
 
 @server.get("/head", response_model=Head)
 def head():
-    return load().head
+    return BUNDLE_MGR.bundle.head
 
 
 @server.get("/")
 def home():
-    saved = load().head
+    saved = BUNDLE_MGR.bundle.head
     return HTMLResponse(
         f"""\
 <html lang="en">
