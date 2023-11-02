394d5cd792c607128845435b2df939fe358f1a46
Tyler Nullmeier <tylerzeromaster@gmail.com>
2023-11-02 16:03:53 -0500

    Update hotdog stack to work with traefik v2

--- scripts/vars.hotdog.sh
+++ scripts/vars.hotdog.sh
@@ -2,7 +2,6 @@
 
 export DOMAIN="corgi-hotdog.ce.openstax.org"
 export STACK_NAME="corgi_hotdog"
-export TRAEFIK_TAG="traefik-hotdog"
 set +x
 SESSION_SECRET="$(dd if=/dev/urandom bs=1024 count=1 2> /dev/null | base64)"
 set -x
@@ -12,5 +11,4 @@ NEWLINE=$'\n'
 echo "${NEWLINE}The following environment variables were set:${NEWLINE}"
 echo "DOMAIN=$DOMAIN"
 echo "STACK_NAME=$STACK_NAME"
-echo "TRAEFIK_TAG=$TRAEFIK_TAG"
 echo "${NEWLINE}"
