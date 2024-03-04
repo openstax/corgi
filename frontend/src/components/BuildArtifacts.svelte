7787995e0a3810e2e3324b1510d3bdf790f304b4
Tyler Nullmeier <tylerzeromaster@gmail.com>
2024-03-04 16:35:08 -0600

    Move build artifacts into new component

---
+++ frontend/src/components/BuildArtifacts.svelte
@@ -0,0 +1,30 @@
+<script lang="ts">
+  import type { Job } from "../ts/types";
+
+  export let selectedJob: Job;
+</script>
+
+<div id="build-artifacts-frame">
+  <h3>Build Artifacts</h3>
+  <ul>
+    {#each selectedJob.artifact_urls as artifact}
+      <li>
+        <a href={artifact.url} target="_blank" rel="noreferrer"
+          >{artifact.slug}</a
+        >
+      </li>
+    {/each}
+  </ul>
+</div>
+
+<style>
+  #build-artifacts-frame {
+    padding: 10px;
+    margin: 10px;
+    background-color: rgba(255, 128, 0, 0.25);
+  }
+
+  #build-artifacts-frame h3:nth-of-type(1) {
+    margin-top: 0;
+  }
+</style>
