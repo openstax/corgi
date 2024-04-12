fbb13bbc4d55a4e83d089e081d598427eb4e19cd
Tyler Nullmeier <tylerzeromaster@gmail.com>
2024-04-12 10:17:54 -0500

    Cleanup deploy scripts
    Add ability to run locally and set ssh host
    
    Specify all variables in deploy command
    
    Stop using docker-compose config before deploying the stack
    
    Use ssh to run docker commands instead of running docker commands via DOCKER_HOST

--- scripts/promote-deploy.sh
+++ scripts/promote-deploy.sh
@@ -1,21 +1,52 @@
 #!/usr/bin/env bash
-set -e
 
-dir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
-staging_fe_tag=$(docker service ls | awk '{ if ($2 == "corgi_stag_frontend") { split($5, image, ":"); print image[2] } }')
-staging_be_tag=$(docker service ls | awk '{ if ($2 == "corgi_stag_backend") { split($5, image, ":"); print image[2] } }')
+# Exit in case of error
+set -euo pipefail
 
-prod_fe_tag=$(docker service ls | awk '{ if ($2 == "corgi_prod_frontend") { split($5, image, ":"); print image[2] } }')
-prod_be_tag=$(docker service ls | awk '{ if ($2 == "corgi_prod_backend") { split($5, image, ":"); print image[2] } }')
+if test -n "${DEBUG_RUN_LOCAL:-}"; then
+    run_script() {
+        if test -n "${SSH_HOST:-}"; then
+            echo "ERROR: SSH_HOST and DEBUG_RUN_LOCAL should not both be set"
+            exit 111
+        fi
+        env -i HOME="$HOME" PATH="$PATH" bash -seuxo pipefail
+    }
+else
+    run_script() {
+        ssh "${SSH_HOST:?}" 'bash -seuo pipefail'
+    }
+fi
 
-echo "Script dir: ${dir}"
+try_find_tag() {
+    service_name="$1"
+    : "${service_name:?}"
+    tag=""
+    while read -r _id name _mode _replicas image _ports; do
+        if [[ "$name" == "$service_name" ]]; then
+            tag="$(echo "$image" | cut -d: -f2)"
+            break
+        fi
+    done
+    echo "$tag"
+}
+
+service_list="$(echo "docker service ls" | run_script)"
+scripts_dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+
+staging_fe_tag=$(echo "$service_list" | try_find_tag "corgi_stag_frontend")
+staging_be_tag=$(echo "$service_list" | try_find_tag "corgi_stag_backend")
+
+prod_fe_tag=$(echo "$service_list" | try_find_tag "corgi_prod_frontend")
+prod_be_tag=$(echo "$service_list" | try_find_tag "corgi_prod_backend")
+
+: "${staging_fe_tag:?}"
+: "${staging_be_tag:?}"
+[[ "$staging_fe_tag" = "$staging_be_tag" ]] || (echo "Err: Frontend and backend tags differ" && exit 3)
+[[ "$staging_fe_tag" =~ ^[0-9]{8}[.][0-9]{6}$ ]] || (echo "Err: Bad format for tag" && exit 4)
+
+echo "Script dir: ${scripts_dir}"
 echo "Detected STAGING tags frontend ($staging_fe_tag) and backend ($staging_be_tag)"
 echo "Detected PROD tags frontend ($prod_fe_tag) and backend ($prod_be_tag)"
-
-[[ -z "$staging_fe_tag" ]] && echo "Err: Empty tag" && exit 1
-[[ -z "$staging_be_tag" ]] && echo "Err: Empty tag" && exit 1
-[[ "$staging_fe_tag" = "$staging_be_tag" ]] || (echo "Err: Frontend and backend tags differ" && exit 2)
-[[ "$staging_fe_tag" =~ ^[0-9]{8}[.][0-9]{6}$ ]] || (echo "Err: Bad format for tag" && exit 3)
 
 echo -n "Promoting in "
 echo -n "5 " && sleep 1
@@ -25,8 +56,11 @@ echo -n "2 " && sleep 1
 echo -n "1 " && sleep 1
 echo
 
-# shellcheck source=SCRIPTDIR/vars.prod.sh
-source "${dir}/vars.prod.sh"
-export TAG=$staging_fe_tag
-# shellcheck source=SCRIPTDIR/deploy.sh
-source "${dir}/deploy.sh"
+
+# shellcheck disable=SC1091
+source "$scripts_dir/vars.common.sh"
+# shellcheck disable=SC1091
+source "$scripts_dir/vars.prod.sh"
+export TAG="$staging_fe_tag"
+# shellcheck disable=SC1091
+source "$scripts_dir/deploy.sh"
