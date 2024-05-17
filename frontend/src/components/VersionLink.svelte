4b599012df1c966cfd67d635c2d93f8ccc40df26
Tyler Nullmeier <tylerzeromaster@gmail.com>
2024-05-17 10:22:58 -0500

    Create ABL dialog
    For viewing all ABL entries
    
    Update ABL store when it is opened
    
    Reduce 'cooldown' on `ABLStore.update` to 2 seconds

---
+++ frontend/src/components/VersionLink.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  export let href: string
+  export let text: string
+</script>
+
+<a
+  data-control-type="link-version"
+  href={href}
+  target="_blank"
+  rel="noreferrer"
+>
+  {text}
+</a>
